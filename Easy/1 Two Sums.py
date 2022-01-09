19fae4183cc1d308e93736f8ce964ad09050bc85
ARNAB DAS <raj713335@gmail.com>
2022-01-09 06:23:00 +0000

    Add files via upload

---
+++ Easy/1 Two Sums.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        diff = {}
+        for index, value in enumerate(nums): 
+            differ = target - value
+            if value in diff:
+                return(index, diff[value])
+            else:
+                diff[differ] = index
+            
+            
+
