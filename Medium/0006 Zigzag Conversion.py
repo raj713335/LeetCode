6e2fee27bc70e7c39ef64e31ee9d87a58e58fe95
ARNAB DAS <raj713335@gmail.com>
2022-02-03 10:17:26 +0530

    Create 0006 Zigzag Conversion.py

---
+++ Medium/0006 Zigzag Conversion.py
@@ -0,0 +1,34 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1:
+            return s
+        
+        l = len(s)
+
+
+        arr=["" for x in range(l)]
+        row = 0
+        
+        for i in range(l):
+
+            arr[row] += s[i]
+
+            if row == numRows - 1:
+                down = False
+
+            elif row == 0:
+                down = True
+
+            if down:
+                row += 1
+            else:
+                row -= 1
+
+        # Print concatenation
+        # of all rows
+        final = ""
+        for i in range(l):
+            final += arr[i]
+        return final
+        
+        
