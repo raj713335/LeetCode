b816ba9bec9924bdaa38fbfffa7f6297e05c9e9a
ARNAB DAS <raj713335@gmail.com>
2022-02-03 10:18:05 +0530

    Create 0007 Reverse Integer.py

---
+++ Medium/0007 Reverse Integer.py
@@ -0,0 +1,15 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        while x%10 == 0 and not x == 0:
+            x = x//10
+        if x < 0:
+            int_num = int("-"+ (str(x)[1:])[::-1])
+        else:
+            int_num = int(str(x)[::-1])
+        
+        
+        
+        if int_num >= -2**31 and int_num <= 2**31-1:
+            return int_num
+        else:
+            return 0
