649a3e237c13c89399a718c9e640d1ab1db5885c
ARNAB DAS <raj713335@gmail.com>
2022-02-04 17:15:38 +0530

    Update 0006 Zigzag Conversion.py

--- Medium/0006 Zigzag Conversion.py
+++ Medium/0006 Zigzag Conversion.py
@@ -1,47 +1,35 @@
 #https://leetcode.com/problems/zigzag-conversion/
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
+        
         if numRows == 1:
             return s
-
-        res = ""
-
+        
         list = ["" for x in range(numRows)]
 
-        #print(list)
-
-        length = len(s)
-        iter = 0
+        iterx = 0
         i = 0
-        flag = False
-
-        while (length!=0):
-
-            if iter<numRows and flag!= True:
-                list[iter]+= s[i]
-                i+=1
-                iter+=1
-                length-=1
-                if iter == numRows-1:
+        flag =False
+        
+        while(i!=len(s)):
+            
+            if iterx < numRows and flag!= True:
+                list[iterx]+= s[i]
+                i+= 1
+                iterx+=1
+           
+                if iterx == numRows-1:
                     flag = True
+                
+            elif flag == True:
 
-            elif flag==True:
-
-                list[iter]+= s[i]
-                iter -= 1
+                list[iterx]+=s[i]
+                iterx -=1
                 i+=1
-                length -= 1
 
 
-                if iter== 0:
+                if iterx ==0:
                     flag = False
-
-
-            #print(list)
-
-
-
-
+                        
         return "".join(list)
-
         
