b70a56748e2aaa0a4a7c1deb2ec4da32ab85edbf
ARNAB DAS <raj713335@gmail.com>
2022-03-03 21:52:18 +0530

    Create 0033 Search in Rotated Sorted Array.py

---
+++ Medium/0033 Search in Rotated Sorted Array.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        try:
+            val = nums.index(target)
+        except:
+            val = -1
+        return val
+        
