47b7862534d82f355e349be649374bbc0b4e45ac
ARNAB DAS <raj713335@gmail.com>
2022-03-13 15:42:39 +0530

    Create 0344 Reverse String.py

---
+++ Easy/0344 Reverse String.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/reverse-string/
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        
+        s[:] = s[::-1]
+        
