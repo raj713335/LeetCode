d090a9576b4372382b0cbdd55a0a75ff278467c5
ARNAB DAS <raj713335@gmail.com>
2022-03-15 01:57:56 +0530

    Create 0136 Single Number.py

---
+++ Easy/0136 Single Number.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/single-number/
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        
+        res = 0
+        for num in nums:
+            res ^= num
+        return res
