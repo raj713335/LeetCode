8b4dc4664abba09842b7ea0a15303f1c497c9111
ARNAB DAS <raj713335@gmail.com>
2022-03-30 23:15:46 +0530

    Create 0461 Hamming Distance.py

---
+++ Easy/0461 Hamming Distance.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/hamming-distance/
+
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        
+        return bin(x^y).count('1')
+        
+        
