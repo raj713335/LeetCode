758af9d1b2fec86f0246d71ca795825093909fd1
ARNAB DAS <raj713335@gmail.com>
2022-07-05 00:40:01 +0530

    Create 1689 Partitioning Into Minimum Number Of Deci-Binary Numbers.py

---
+++ Medium/1689 Partitioning Into Minimum Number Of Deci-Binary Numbers.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/partitioning-into-minimum-number-of-deci-binary-numbers/
+
+class Solution:
+    def minPartitions(self, n: str) -> int:
+
+        return int(max(n))
+        
