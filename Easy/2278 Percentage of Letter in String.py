e8ae3dced1a796e9deaa2400824b7cc310c89481
ARNAB DAS <raj713335@gmail.com>
2022-07-09 21:32:51 +0530

    Create 2278 Percentage of Letter in String.py

---
+++ Easy/2278 Percentage of Letter in String.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/percentage-of-letter-in-string/
+
+
+class Solution:
+    def percentageLetter(self, s: str, letter: str) -> int:
+        
+        return int((s.count(letter)/len(s))*100)
+        
