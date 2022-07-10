a16ddf2e979625eeeb1a816545a0cbf5848226f8
ARNAB DAS <raj713335@gmail.com>
2022-07-11 04:13:32 +0530

    Create 0217 Contains Duplicate.py

---
+++ Easy/0217 Contains Duplicate.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/contains-duplicate/
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        
+        if len(nums) == len(set(nums)):
+            return False
+        else:
+            return True
