4012fcd37349d0a34e8967c4dd13de3e1bbe7ea3
ARNAB DAS <raj713335@gmail.com>
2022-07-16 20:51:00 +0530

    Create 1472 Design Browser History.py

---
+++ Medium/1472 Design Browser History.py
@@ -0,0 +1,33 @@
+# https://leetcode.com/problems/design-browser-history/
+
+
+class BrowserHistory:
+
+    def __init__(self, homepage: str):
+        self.history = [homepage]
+        self.curr = 0
+        self.bound = 0
+
+    def visit(self, url: str) -> None:
+        self.curr += 1
+        if self.curr == len(self.history):
+            self.history.append(url)
+        else:
+            self.history[self.curr] = url
+        self.bound = self.curr
+
+    def back(self, steps: int) -> str:
+        self.curr = max(self.curr - steps, 0)
+        return self.history[self.curr]
+
+    def forward(self, steps: int) -> str:
+        self.curr = min(self.curr + steps, self.bound)
+        return self.history[self.curr]
+        
+
+
+# Your BrowserHistory object will be instantiated and called as such:
+# obj = BrowserHistory(homepage)
+# obj.visit(url)
+# param_2 = obj.back(steps)
+# param_3 = obj.forward(steps)
