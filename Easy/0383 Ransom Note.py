f9e51e27f5cbca3c4d9fd12deae419d3f1636983
ARNAB DAS <raj713335@gmail.com>
2022-08-04 15:11:18 +0530

    Create 0383 Ransom Note.py

---
+++ Easy/0383 Ransom Note.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/ransom-note/
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        
+        dictx = {}
+        
+        for each in magazine:
+            if each not in dictx:
+                dictx[each] = 1
+            else:
+                dictx[each] += 1
+                
+        for each in ransomNote:
+            if each not in dictx:
+                return False
+            elif dictx[each] < 1:
+                return False
+            else:
+                dictx[each] -=1
+            
+        return True
+        
