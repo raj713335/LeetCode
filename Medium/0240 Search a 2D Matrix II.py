fa5939c3db6a6526ff0d5b734efd4ae17bb6a064
ARNAB DAS <raj713335@gmail.com>
2022-08-06 08:44:52 +0530

    Create 0240 Search a 2D Matrix II.py

---
+++ Medium/0240 Search a 2D Matrix II.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/search-a-2d-matrix-ii/
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        
+        for i in range(0, len(matrix)):
+            for j in range(0, len(matrix[0])):
+                if matrix[i][j] == target:
+                    return True
+        return False
+        
