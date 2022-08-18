3692712691499c91d3a20f8e53cbaa5bde4c032d
ARNAB DAS <raj713335@gmail.com>
2022-08-19 05:19:30 +0530

    Create 0225 Implement Stack using Queues.py

---
+++ Easy/0225 Implement Stack using Queues.py
@@ -0,0 +1,40 @@
+# https://leetcode.com/problems/implement-stack-using-queues/
+
+
+class MyStack:
+
+    def __init__(self):
+        self.stack = []
+        
+        
+        
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+        
+
+    def pop(self) -> int:
+        if len(self.stack) > 0:
+            return self.stack.pop()
+        
+
+    def top(self) -> int:
+        if len(self.stack) > 0:
+            return self.stack[-1]
+        
+        
+
+    def empty(self) -> bool:
+        if len(self.stack) > 0:
+            return False
+        else:
+            return True
+        
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
