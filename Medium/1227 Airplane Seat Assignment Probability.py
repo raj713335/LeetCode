155bdf79c47c26ed381b992d74e6a474014112a9
ARNAB DAS <raj713335@gmail.com>
2022-08-19 03:50:13 +0530

    Create 1227 Airplane Seat Assignment Probability.py

---
+++ Medium/1227 Airplane Seat Assignment Probability.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/airplane-seat-assignment-probability/
+
+
+class Solution:
+    def nthPersonGetsNthSeat(self, n: int) -> float:
+        
+        if n == 1:
+            return 1
+        return 0.5
+        
