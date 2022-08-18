66d0bc8a7f2c4cd2229c583205a16ef322824302
ARNAB DAS <raj713335@gmail.com>
2022-08-19 05:10:51 +0530

    Create 0100 Same Tree.py

---
+++ Easy/0100 Same Tree.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/same-tree/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        
+        if p is None and q is None:
+            return True
+        if p is None and q is not None:
+            return False
+        if p is not None and q is None:
+            return False
+        if p is not None and q is not None:
+            return self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right) and p.val==q.val
+        
+        
