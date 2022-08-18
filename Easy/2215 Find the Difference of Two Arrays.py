7a12530f343952097af0168ae34ad8271a3be114
ARNAB DAS <raj713335@gmail.com>
2022-08-19 04:15:13 +0530

    Create 2215 Find the Difference of Two Arrays.py

---
+++ Easy/2215 Find the Difference of Two Arrays.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/find-the-difference-of-two-arrays/
+
+
+class Solution:
+    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        
+        dictx1 = {}
+        dictx2 = {}
+        
+        output = [[], []]
+        
+        for each in set(nums1):
+            dictx1[each] = 1
+            
+        for each in set(nums2):
+            dictx2[each] = 1
+            
+        for key, val in dictx1.items():
+            if key not in dictx2:
+                output[0].append(key)
+                
+        for key, val in dictx2.items():
+            if key not in dictx1:
+                output[1].append(key)
+                
+        return output
+        
