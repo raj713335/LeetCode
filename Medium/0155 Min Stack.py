24e14885f78d929d083d24b045155d2596825f0b
ARNAB DAS <raj713335@gmail.com>
2022-08-27 01:06:13 +0530

    Create 0155 Min Stack.py

---
+++ Medium/0155 Min Stack.py
@@ -0,0 +1,33 @@
+# https://leetcode.com/problems/min-stack/
+
+
+class MinStack:
+
+    def __init__(self):
+        self.value = []
+        
+
+    def push(self, val: int) -> None:
+        self.value.append(val)
+        
+
+    def pop(self) -> None:
+        self.value.pop()
+        
+
+    def top(self) -> int:
+        top = self.value[-1]
+        return top
+        
+
+    def getMin(self) -> int:
+        return min(self.value)
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
