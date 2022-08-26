2ef48b250d2fa47b2b4045aa4ade170cd5d8da45
ARNAB DAS <raj713335@gmail.com>
2022-08-27 01:16:17 +0530

    Create 0215 Kth Largest Element in an Array.py

---
+++ Medium/0215 Kth Largest Element in an Array.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/kth-largest-element-in-an-array/
+
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        
+        return sorted(nums)[-k]
+        
