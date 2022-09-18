bd69b2b7a991d74a4dc4a4d02850234fd3747a78
ARNAB DAS <raj713335@gmail.com>
2022-09-18 23:49:08 +0530

    Create 0783 Minimum Distance Between BST Nodes.py

---
+++ Easy/0783 Minimum Distance Between BST Nodes.py
@@ -0,0 +1,34 @@
+# https://leetcode.com/problems/minimum-distance-between-bst-nodes/
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDiffInBST(self, root: Optional[TreeNode]) -> int:
+        
+        stack =[]
+
+        def printInorder(root):
+
+            if root:
+                printInorder(root.left)
+                stack.append(root.val)
+                printInorder(root.right)
+
+            return sorted(stack)    
+
+        arr = sorted(printInorder(root))
+
+
+        minx = math.inf
+
+        for i in range(0, len(arr)-1):
+            if arr[i+1] - arr[i] < minx:
+                minx = arr[i+1] - arr[i]
+
+        return minx
+        
