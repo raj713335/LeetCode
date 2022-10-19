e77d97f58629573b906ff850c6214fa1636c152a
ARNAB DAS <raj713335@gmail.com>
2022-10-19 19:18:01 +0530

    Create 0171 Excel Sheet Column Number.py

---
+++ Easy/0171 Excel Sheet Column Number.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/excel-sheet-column-number/
+
+
+class Solution:
+    def titleToNumber(self, columnTitle: str) -> int:
+        
+        res = 0
+        for c in columnTitle:
+            res = res*26 + ord(c)-ord('A')+1
+        return res
+            
