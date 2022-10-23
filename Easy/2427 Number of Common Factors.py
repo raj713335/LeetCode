92095b8ea85d7d67fc98c8dc8932a375488b60fa
ARNAB DAS <raj713335@gmail.com>
2022-10-23 16:03:14 +0530

    Create 2427 Number of Common Factors.py

---
+++ Easy/2427 Number of Common Factors.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/number-of-common-factors/
+
+
+class Solution:
+    def commonFactors(self, a: int, b: int) -> int:
+        
+        count = 0
+        
+        for i in range(1, min(a,b)+1):
+            if a%i == 0 and b%i == 0:
+                count += 1
+                
+                
+        return count
+        
