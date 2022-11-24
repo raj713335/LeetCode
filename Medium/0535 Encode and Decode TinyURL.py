d52080a6a36e049439481794084641c9ff46d6c9
ARNAB DAS <raj713335@gmail.com>
2022-11-25 03:32:06 +0530

    Create 0535 Encode and Decode TinyURL.py

---
+++ Medium/0535 Encode and Decode TinyURL.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/encode-and-decode-tinyurl/description/
+
+
+class Codec:
+
+    codeDB, urlDB = defaultdict(), defaultdict()
+    chars = string.ascii_letters + string.digits
+
+    def getCode(self) -> str:
+        code = ''.join(random.choice(self.chars) for i in range(6))
+        return "http://tinyurl.com/" + code
+ 
+    def encode(self, longUrl: str) -> str:
+        if longUrl in self.urlDB: return self.urlDB[longUrl]
+        code = self.getCode()
+        while code in self.codeDB: code = getCode()
+        self.codeDB[code] = longUrl
+        self.urlDB[longUrl] = code
+        return code
+
+    def decode(self, shortUrl: str) -> str:
+        return self.codeDB[shortUrl]
+        
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(url))
