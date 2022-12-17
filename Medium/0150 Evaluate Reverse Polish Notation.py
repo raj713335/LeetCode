b34da471f01808c3d7942e730b7a3b3de655ff3b
ARNAB DAS <raj713335@gmail.com>
2022-12-17 12:13:23 +0530

    Create 0150 Evaluate Reverse Polish Notation.py

---
+++ Medium/0150 Evaluate Reverse Polish Notation.py
@@ -0,0 +1,28 @@
+# https://leetcode.com/problems/evaluate-reverse-polish-notation/description/
+
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+
+        stack = []
+
+        for each in tokens:
+            try:
+                val = int(each)
+                stack.append(val)
+            except:
+                a = stack.pop()
+                b = stack.pop()
+                if each == "+":
+                    val = int(b) + int(a)
+                elif each == "-":
+                    val = int(b) - int(a)
+                elif each == "*":
+                    val = int(b) * int(a)
+                else:
+                    val = int(b) / int(a)
+                stack.append(val)
+
+        return int(stack[0])
+
+                 
