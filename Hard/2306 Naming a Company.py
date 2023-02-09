fb20e9dca5765d79b8cb3d5e00370ef7f01798ee
ARNAB DAS <raj713335@gmail.com>
2023-02-10 02:33:55 +0530

    Create 2306 Naming a Company.py

---
+++ Hard/2306 Naming a Company.py
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/naming-a-company/description/
+
+
+
+class Solution:
+    def distinctNames(self, ideas: List[str]) -> int:
+
+        wordMap = collections.defaultdict(set)
+        for w in ideas:
+            wordMap[w[0]].add(w[1:])
+        
+        res = 0
+
+
+        for char1 in wordMap:
+            for char2 in wordMap:
+                if char1 == char2:
+                    continue
+                intersect = 0
+                for w in wordMap[char1]:
+                    if w in wordMap[char2]:
+                        intersect += 1
+
+                distinct1 = len(wordMap[char1]) - intersect
+                distinct2 = len(wordMap[char2]) - intersect
+
+                res += distinct1 * distinct2
+
+        return res
