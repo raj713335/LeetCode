0f3e5030b931307fb0e8675d39da80e267572053
ARNAB DAS <raj713335@gmail.com>
2023-02-12 13:57:43 +0530

    Create 1370 Increasing Decreasing String.py

---
+++ Easy/1370 Increasing Decreasing String.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/increasing-decreasing-string/
+
+
+class Solution:
+    def sortString(self, s: str) -> str:
+        s = list(s)
+        result = ''
+        while s:
+            for letter in sorted(set(s)):
+                s.remove(letter)
+                result += letter
+            for letter in sorted(set(s), reverse=True):
+                s.remove(letter)
+                result += letter
+        return result
