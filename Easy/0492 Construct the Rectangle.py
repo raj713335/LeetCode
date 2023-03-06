376a7f4444b3c64386b72516fa876e973fcddcaf
ARNAB DAS <raj713335@gmail.com>
2023-03-07 04:28:15 +0530

    Create 0492 Construct the Rectangle.py

---
+++ Easy/0492 Construct the Rectangle.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/construct-the-rectangle/description/
+
+
+class Solution:
+    def constructRectangle(self, area: int) -> List[int]:
+
+        for l in range(int(area**0.5), 0, -1):            
+            if area % l == 0: 
+                return [area // l, l]
