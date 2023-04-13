36799c791a5f78ed3324a05ad20fc66e77e185b3
ARNAB DAS <raj713335@gmail.com>
2023-04-13 07:35:56 +0530

    Update and rename 2621 Sleep.py to 2621 Sleep.ts

--- Easy/2621 Sleep.ts
+++ Easy/2621 Sleep.ts
@@ -1,4 +1,4 @@
-# https://leetcode.com/problems/sleep/description/
+// https://leetcode.com/problems/sleep/description/
 
 async function sleep(millis: number): Promise<void> {
     return new Promise( resolve => setTimeout(resolve, millis) );
