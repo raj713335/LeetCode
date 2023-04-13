150b820ccac6eab0950db7dff70847726a254502
ARNAB DAS <raj713335@gmail.com>
2023-04-13 07:18:08 +0530

    Create 2620 Counter.py

---
+++ Easy/2620 Counter.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/counter/description/
+
+function createCounter(n: number): () => number {
+    return function() {
+        return n++;
+    }
+}
+
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
