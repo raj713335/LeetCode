ad5de21c65fe847c6fd01aa3899538290bd31215
ARNAB DAS <raj713335@gmail.com>
2023-04-13 07:55:01 +0530

    Create 2619 Array Prototype Last.ts

---
+++ Easy/2619 Array Prototype Last.ts
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/apply-transform-over-each-element-in-array/description/
+
+function map(arr: number[], fn: (n: number, i: number) => number): number[] {
+
+    for (let i=0; i<arr.length; i++){
+        arr[i] = fn(arr[i], i);
+    }
+    return arr;
+
+};
