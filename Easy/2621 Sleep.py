ec1875dd5b50ced37e721b2eb09a3cbee9f00fbb
ARNAB DAS <raj713335@gmail.com>
2023-04-13 07:21:28 +0530

    Create 2621 Sleep.py

---
+++ Easy/2621 Sleep.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/sleep/description/
+
+async function sleep(millis: number): Promise<void> {
+    return new Promise( resolve => setTimeout(resolve, millis) );
+    
+}
+
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
