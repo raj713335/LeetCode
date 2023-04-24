13fa6dc6e12115f0b027e39d08ff7b495bd2c2a8
ARNAB DAS <raj713335@gmail.com>
2023-04-24 17:14:13 +0530

    Create 2651 Calculate Delayed Arrival Time.py

---
+++ Easy/2651 Calculate Delayed Arrival Time.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/calculate-delayed-arrival-time/
+
+
+class Solution:
+    def findDelayedArrivalTime(self, arrivalTime: int, delayedTime: int) -> int:
+
+        return (arrivalTime+delayedTime)%24
