bce1f4185709c69a106596bfeeee1285ba721507
ARNAB DAS <raj713335@gmail.com>
2023-05-05 15:56:19 +0530

    Create 1456 Maximum Number of Vowels in a Substring of Given Length.py

---
+++ Medium/1456 Maximum Number of Vowels in a Substring of Given Length.py
@@ -0,0 +1,33 @@
+# https://leetcode.com/problems/maximum-number-of-vowels-in-a-substring-of-given-length/
+
+
+class Solution:
+    def maxVowels(self, s: str, k: int) -> int:
+
+        max_substring = 0
+
+        dictx = {"a": 0, "e": 0, "i": 0, "o": 0, "u": 0}
+
+        sub = s[0:k]
+        temp = 0
+        for each in sub:
+            if each in dictx:
+                temp += 1
+
+        if temp > max_substring:
+            max_substring = temp
+
+        for i in range(k, len(s)):
+
+            if sub[0] in dictx:
+                temp -= 1
+
+            sub = sub[1:] + s[i]
+
+            if s[i] in dictx:
+                temp += 1
+
+            if temp > max_substring:
+                max_substring = temp
+
+        return max_substring
