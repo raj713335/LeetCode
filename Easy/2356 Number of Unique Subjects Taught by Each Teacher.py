50427fa1a8af98f030a44a226f46344289fd7037
ARNAB DAS <raj713335@gmail.com>
2023-05-07 19:32:34 +0530

    Create 2356 Number of Unique Subjects Taught by Each Teacher.py

---
+++ Easy/2356 Number of Unique Subjects Taught by Each Teacher.py
@@ -0,0 +1,6 @@
+# https://leetcode.com/problems/number-of-unique-subjects-taught-by-each-teacher/description/
+
+# Write your MySQL query statement below
+
+SELECT teacher_id,COUNT(DISTINCT subject_id) as cnt FROM Teacher
+GROUP BY teacher_id;
