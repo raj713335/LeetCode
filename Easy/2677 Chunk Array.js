3af309c12a067ed4a530ee127259f4fc9b1fb34a
ARNAB DAS <raj713335@gmail.com>
2023-05-14 12:19:46 +0530

    Create 2677 Chunk Array.js

---
+++ Easy/2677 Chunk Array.js
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/chunk-array/description/
+
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array[]}
+ */
+const chunk = (arr, size) =>
+  Array.from({ length: Math.ceil(arr.length / size) }, (_, index) =>
+    arr.slice(index * size, index * size + size)
+  );
