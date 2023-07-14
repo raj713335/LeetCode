2d50bf0ab612f73ba852b7d57b76a038f7cb82bf
ARNAB DAS <raj713335@gmail.com>
2023-07-14 20:56:59 +0530

    Create 2703 Return Length of Arguments Passed.py

---
+++ Easy/2703 Return Length of Arguments Passed.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/return-length-of-arguments-passed/
+
+/**
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return args.length;
+ 
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
