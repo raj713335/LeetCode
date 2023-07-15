b10031d115dda00f514862aba947dda92c9ccd1a
ARNAB DAS <raj713335@gmail.com>
2023-07-15 09:02:17 +0530

    Create 2725 Interval Cancellation.ts

---
+++ Easy/2725 Interval Cancellation.ts
@@ -0,0 +1,49 @@
+# https://leetcode.com/problems/interval-cancellation/description/
+
+function cancellable(fn: Function, args: any[], t: number): Function {
+    
+    // support variables
+    let shouldRepeat = true;
+    // helper function
+    const repeat = () => {
+        fn(...args);
+        setTimeout(() => {
+            shouldRepeat && repeat();
+        }, t);
+    }
+    // booting repeat up
+    repeat();
+    return () => shouldRepeat = false;
+
+};
+
+/**
+ *  const result = []
+ *
+ *  const fn = (x) => x * 2
+ *  const args = [4], t = 20, cancelT = 110
+ *
+ *  const start = performance.now()
+ *
+ *  const log = (...argsArr) => {
+ *      const diff = Math.floor(performance.now() - start)
+ *      result.push({"time": diff, "returned": fn(...argsArr)})
+ *  }
+ *       
+ *  const cancel = cancellable(log, args, t);
+ *
+ *  setTimeout(() => {
+ *     cancel()
+ *  }, cancelT)
+ *   
+ *  setTimeout(() => {
+ *    console.log(result)  // [
+ *                         //      {"time":0,"returned":8},
+ *                         //      {"time":20,"returned":8},
+ *                         //      {"time":40,"returned":8},           
+ *                         //      {"time":60,"returned":8},
+ *                         //      {"time":80,"returned":8},
+ *                         //      {"time":100,"returned":8}
+ *                         //  ]
+ *  }, cancelT + t + 15)    
+ */
