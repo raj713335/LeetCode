e2f327d875fbd70ce9726c28d6e9698b1d1cd1e9
ARNAB DAS <raj713335@gmail.com>
2023-07-25 09:11:31 +0530

    Create 0852 Peak Index in a Mountain Array.py

---
+++ Medium/0852 Peak Index in a Mountain Array.py
@@ -0,0 +1,5 @@
+# https://leetcode.com/problems/peak-index-in-a-mountain-array/
+
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        return arr.index(max(arr))
