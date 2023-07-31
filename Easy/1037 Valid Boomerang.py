0b641130ae07b33a283ad8900c65fbc025fbc420
ARNAB DAS <raj713335@gmail.com>
2023-08-01 00:45:23 +0530

    Create 1037 Valid Boomerang.py

---
+++ Easy/1037 Valid Boomerang.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/valid-boomerang/description/
+
+class Solution:
+    def isBoomerang(self, points: List[List[int]]) -> bool:
+        a,b,c=points
+        return (b[1]-a[1])*(c[0]-b[0]) != (c[1]-b[1])*(b[0]-a[0])
+        
