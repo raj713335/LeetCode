2e39553068a244e9b9797b511e752caa753b6ff9
ARNAB DAS <raj713335@gmail.com>
2023-08-02 20:48:24 +0530

    Create 1517 Find Users With Valid E-Mails.sql

---
+++ Easy/1517 Find Users With Valid E-Mails.sql
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/find-users-with-valid-e-mails/
+
+# Write your MySQL query statement below
+
+SELECT
+  *
+FROM
+  Users
+WHERE
+  mail REGEXP '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode[.]com$';
