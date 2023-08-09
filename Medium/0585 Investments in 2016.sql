923579086b7eeb4bd2e898b99070ad43f1142c4c
ARNAB DAS <raj713335@gmail.com>
2023-08-09 09:31:08 +0530

    Create 0585 Investments in 2016.sql

---
+++ Medium/0585 Investments in 2016.sql
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/investments-in-2016/
+
+# Write your MySQL query statement below
+
+SELECT ROUND(SUM(tiv_2016), 2) as tiv_2016 
+FROM Insurance I1
+WHERE tiv_2015 IN (SELECT tiv_2015 FROM INSURANCE I2
+    WHERE I1.pid != I2.pid)
+AND
+(lat, lon) NOT IN (SELECT lat, lon FROM INSURANCE I3
+    WHERE I1.pid != I3.pid)
