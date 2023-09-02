5623e96a706fd2b2084555183738c6cadad627c3
ARNAB DAS <raj713335@gmail.com>
2023-09-03 01:34:36 +0530

    Create 2839 Check if Strings Can be Made Equal With Operations I.py

---
+++ Easy/2839 Check if Strings Can be Made Equal With Operations I.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/check-if-strings-can-be-made-equal-with-operations-i/description/
+
+
+class Solution:
+    def canBeEqual(self, s1: str, s2: str) -> bool:
+
+        return ({s1[0], s1[2]} == {s2[0], s2[2]} and
+                {s1[1], s1[3]} == {s2[1], s2[3]})
+    
+        
