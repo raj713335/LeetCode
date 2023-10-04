c34d9a3826091757eb2c9d4c051e78faf848d831
ARNAB DAS <raj713335@gmail.com>
2023-10-05 00:58:14 +0530

    Create 2881 Create a New Column.py

---
+++ Easy/2881 Create a New Column.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/create-a-new-column/description/
+
+import pandas as pd
+
+def createBonusColumn(employees: pd.DataFrame) -> pd.DataFrame:
+
+    employees['bonus'] = employees['salary'] * 2
+
+    return employees
+    
