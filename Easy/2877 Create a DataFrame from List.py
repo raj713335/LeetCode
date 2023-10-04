c0ff0741b8f694f68598b077c51557efcf5af6af
ARNAB DAS <raj713335@gmail.com>
2023-10-04 23:09:37 +0530

    Create 2877 Create a DataFrame from List.py

---
+++ Easy/2877 Create a DataFrame from List.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/create-a-dataframe-from-list/description/
+
+import pandas as pd
+
+def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
+    return pd.DataFrame(student_data, columns =[ 'student_id', 'age' ])
+    
