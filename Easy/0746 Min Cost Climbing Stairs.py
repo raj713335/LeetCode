41194bdb7512458a2188c65687a329576f6bff14
ARNAB DAS <raj713335@gmail.com>
2023-12-06 16:02:51 +0530

    Create 0746 Min Cost Climbing Stairs.py

---
+++ Easy/0746 Min Cost Climbing Stairs.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/min-cost-climbing-stairs/
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+
+        cost.append(0)
+
+        for i in range(len(cost)-3, -1, -1):
+            cost[i] += min(cost[i+1], cost[i+2])
+
+        return min(cost[0], cost[1])
+        
