36f082329d55668238c463545d41dc0fc6411d3b
ARNAB DAS <raj713335@gmail.com>
2023-12-07 01:12:10 +0530

    Create 0696 Count Binary Substrings.py

---
+++ Easy/0696 Count Binary Substrings.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/count-binary-substrings/description/
+
+from itertools import combinations
+
+class Solution:
+    def countBinarySubstrings(self, s: str) -> int:
+
+        s = list(map(len, s.replace('01', '0 1').replace('10', '1 0').split()))
+        return sum(min(a, b) for a, b in zip(s, s[1:]))
+        
+        
