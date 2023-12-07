5e6a9be866f09cc424390e24856d0aa7867ee673
ARNAB DAS <raj713335@gmail.com>
2023-12-08 00:05:01 +0530

    Create 0590 N-ary Tree Postorder Traversal.py

---
+++ Easy/0590 N-ary Tree Postorder Traversal.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/n-ary-tree-postorder-traversal/description/
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        res = []
+        if root == None: return res
+
+        stack = [root]
+        while stack:
+            curr = stack.pop()
+            res.append(curr.val)
+            stack.extend(curr.children)
+
+        return res[::-1]
+        
