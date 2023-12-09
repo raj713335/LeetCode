9b7f7bee84bfc02350a2ff89e9aeb9a46dbf79c2
ARNAB DAS <raj713335@gmail.com>
2023-12-09 09:45:43 +0530

    Create 0404 Sum of Left Leaves.py

---
+++ Easy/0404 Sum of Left Leaves.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/sum-of-left-leaves/description/
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        
+        if not root:
+            return 0
+
+        # does this node have a left child which is a leaf?
+        if root.left and not root.left.left and not root.left.right:
+            return root.left.val + self.sumOfLeftLeaves(root.right)
+
+        # no it does not have a left child or it's not a leaf
+        else:
+			# bummer
+            return self.sumOfLeftLeaves(root.left) + self.sumOfLeftLeaves(root.right)
+        
