7ceb387a281edde963aa73df3c06b593ca8b7508
ARNAB DAS <raj713335@gmail.com>
2024-02-22 16:24:19 +0530

    Create 0201 Bitwise AND of Numbers Range.py

---
+++ Medium/0201 Bitwise AND of Numbers Range.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/bitwise-and-of-numbers-range/description/
+
+class Solution:
+    def rangeBitwiseAnd(self, left: int, right: int) -> int:
+        cnt = 0
+        while left != right:
+            left >>= 1
+            right >>= 1
+            cnt += 1
+        return left << cnt
+        
