45e1942e2a73d43eb65acb51df8d9c2d6a376bf8
ARNAB DAS <raj713335@gmail.com>
2024-04-29 12:54:42 +0530

    Create 2668 Find Latest Salaries.sql

---
+++ Easy/2668 Find Latest Salaries.sql
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/find-latest-salaries/description/
+
+# Write your MySQL query statement below
+
+SELECT emp_id, firstname, lastname, MAX(salary) AS salary, department_id
+FROM 
+Salary
+GROUP BY emp_id
+ORDER BY emp_id
