de91eee1ae21d2664fd19a5886ffcce07330252a
ARNAB DAS <raj713335@gmail.com>
2024-04-30 11:36:50 +0530

    Create 1445 Apples Oranges.sql

---
+++ Medium/1445 Apples Oranges.sql
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/apples-oranges/description/
+
+
+# Write your MySQL query statement below
+
+SELECT
+    sale_date,
+    SUM(IF(fruit = 'apples', sold_num, -sold_num)) AS diff
+FROM Sales
+GROUP BY sale_date
