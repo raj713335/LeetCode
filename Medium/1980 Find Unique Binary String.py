130a0ed2bff66c05e2939e24cfefa0742e19a33e
ARNAB DAS <raj713335@gmail.com>
2024-05-01 17:37:34 +0530

    Create 1980 Find Unique Binary String.py

---
+++ Medium/1980 Find Unique Binary String.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/find-unique-binary-string/description/
+
+class Solution:
+    def findDifferentBinaryString(self, nums: List[str]) -> str:
+
+        res = []
+        for i in range(len(nums)):
+            if nums[i][i] == '1':
+                res.append('0')
+            else:
+                res.append('1')
+        return "".join(res)
+        
