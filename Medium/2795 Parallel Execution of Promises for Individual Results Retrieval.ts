8634c3185fbc57d04c26725f07a0eb741dfd1fe9
ARNAB DAS <raj713335@gmail.com>
2024-05-03 19:52:54 +0530

    Create 2795 Parallel Execution of Promises for Individual Results Retrieval.ts

---
+++ Medium/2795 Parallel Execution of Promises for Individual Results Retrieval.ts
@@ -0,0 +1,52 @@
+# https://leetcode.com/problems/parallel-execution-of-promises-for-individual-results-retrieval/submissions/1248298637/
+
+type FulfilledObj = {
+    status: 'fulfilled';
+    value: string;
+}
+type RejectedObj = {
+    status: 'rejected';
+    reason: string;
+}
+type Obj = FulfilledObj | RejectedObj;
+
+function promiseAllSettled(functions: Function[]): Promise<Obj[]> {
+
+    return new Promise((resolve) => {
+        const results = [];
+        let settledCount = 0;
+
+        functions.forEach((promise, index) => {
+            promise()
+                .then(value => {
+                    results[index] = { status: "fulfilled", value };
+                })
+                .catch(reason => {
+                    results[index] = { status: "rejected", reason };
+                })
+                .finally(() => {
+                    settledCount++;
+
+                    if (settledCount === functions.length) {
+                        resolve(results);
+                    }
+                });
+        });
+    });
+    
+};
+
+
+/**
+ * const functions = [
+ *    () => new Promise(resolve => setTimeout(() => resolve(15), 100))
+ * ]
+ * const time = performance.now()
+ *
+ * const promise = promiseAllSettled(functions);
+ *              
+ * promise.then(res => {
+ *     const out = {t: Math.floor(performance.now() - time), values: res}
+ *     console.log(out) // {"t":100,"values":[{"status":"fulfilled","value":15}]}
+ * })
+ */
