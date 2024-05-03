a45521d570e8a70e450818618e8e2a65be1bbe6c
ARNAB DAS <raj713335@gmail.com>
2024-05-03 19:49:25 +0530

    Create 2632 Curry.ts

---
+++ Medium/2632 Curry.ts
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/curry/
+
+function curry(fn: Function): Function {
+    
+    return function curried(...args: any[]) {
+    if (args.length >= fn.length) {
+      return fn.apply(this, args);
+    }
+    return function (...innerArgs: any[]) {
+      return curried.apply(this, args.concat(innerArgs));
+    };
+  };
+};
+
+/**
+ * function sum(a, b) { return a + b; }
+ * const csum = curry(sum);
+ * csum(1)(2) // 3
+ */
