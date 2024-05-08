833afee15fe2390f8343423fdd1ff3dd04df51a9
ARNAB DAS <raj713335@gmail.com>
2024-05-09 00:43:44 +0530

    Create 1490 Clone N-ary Tree.py

---
+++ Medium/1490 Clone N-ary Tree.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/clone-n-ary-tree/description/
+
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children if children is not None else []
+"""
+
+class Solution:
+    def cloneTree(self, root: 'Node') -> 'Node':
+        return deepcopy(root)
