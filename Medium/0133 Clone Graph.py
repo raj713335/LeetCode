d0adc5dca5d3f1852af9e8eabdf2fa71b3788c46
ARNAB DAS <raj713335@gmail.com>
2024-05-09 00:47:30 +0530

    Create 0133 Clone Graph.py

---
+++ Medium/0133 Clone Graph.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/clone-graph/description/
+
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+from typing import Optional
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        return deepcopy(node)
+        
