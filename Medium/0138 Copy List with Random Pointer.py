c7f6e4e9e93f0db4814444db7f0ec4cb184fa830
ARNAB DAS <raj713335@gmail.com>
2024-05-09 00:48:36 +0530

    Create 0138 Copy List with Random Pointer.py

---
+++ Medium/0138 Copy List with Random Pointer.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/copy-list-with-random-pointer/description/
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        return deepcopy(head)
+        
