83633185645c45568e1c42e464e38827cf718cb2
ARNAB DAS <raj713335@gmail.com>
2024-05-11 03:23:37 +0530

    Create 0362 Design Hit Counter.py

---
+++ Medium/0362 Design Hit Counter.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/design-hit-counter/description/
+
+class HitCounter:
+
+    def __init__(self):
+        self.queue = deque()
+        
+
+    def hit(self, timestamp: int) -> None:
+        self.queue.append(timestamp)
+        
+
+    def getHits(self, timestamp: int) -> int:
+        while self.queue and timestamp - self.queue[0] >= 300:
+            self.queue.popleft()
+
+        return len(self.queue)
+        
+
+
+# Your HitCounter object will be instantiated and called as such:
+# obj = HitCounter()
+# obj.hit(timestamp)
+# param_2 = obj.getHits(timestamp)
