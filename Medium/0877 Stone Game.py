da2a2c8fcc5a7e6b7e125eed306f78194ef411df
ARNAB DAS <raj713335@gmail.com>
2024-06-25 14:51:09 +0530

    Create 0877 Stone Game.py

---
+++ Medium/0877 Stone Game.py
@@ -0,0 +1,35 @@
+# https://leetcode.com/problems/stone-game/description/
+
+
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+
+        # i = 0
+        # alice = 0
+        # bob = 0
+
+        # while piles:
+        #     if i % 2 == 0:
+        #         if piles[0] > piles[-1]:
+        #             alice += piles[0]
+        #             del piles[0]
+        #         else:
+        #             alice += piles[-1]
+        #             del piles[-1]
+        #     else:
+        #         if piles[0] > piles[-1]:
+        #             bob += piles[0]
+        #             del piles[0]
+        #         else:
+        #             bob += piles[-1]
+        #             del piles[-1]
+            
+        #     i += 1
+
+        # if alice > bob:
+        #     return True
+        # else:
+        #     return False
+
+        return True
+        
