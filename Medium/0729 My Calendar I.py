c6a65b18c66f54c4831c3bb367e4c9a19fab4d50
ARNAB DAS <raj713335@gmail.com>
2024-09-26 18:55:16 +0530

    Create 0729 My Calendar I.py

---
+++ Medium/0729 My Calendar I.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/my-calendar-i/description/
+
+class MyCalendar:
+
+    def __init__(self):
+        self.events = []
+        
+
+    def book(self, start: int, end: int) -> bool:
+        for s, e in self.events:
+            if not(e <= start or end <= s):
+                return False
+        self.events.append((start, end))
+        return True
+
+
+# Your MyCalendar object will be instantiated and called as such:
+# obj = MyCalendar()
+# param_1 = obj.book(start,end)
