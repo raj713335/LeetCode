9a5160799e198f53123e7def0533b803ab00eee2
ARNAB DAS <raj713335@gmail.com>
2024-09-27 12:58:08 +0530

    Create 0731 My Calendar II.py

---
+++ Medium/0731 My Calendar II.py
@@ -0,0 +1,28 @@
+# https://leetcode.com/problems/my-calendar-ii/description/
+
+
+class MyCalendarTwo:
+
+    def __init__(self):
+        self.non_overlapping = []
+        self.overlapping = []
+        
+
+    def book(self, start: int, end: int) -> bool:
+
+        for s, e in self.overlapping:
+            if not(e <= start or end <= s):
+                return False
+
+        for s, e in self.non_overlapping:
+            if end > s and e > start:
+                self.overlapping.append((max(s, start), min(e, end)))
+
+        self.non_overlapping.append((start, end))
+        return True
+        
+
+
+# Your MyCalendarTwo object will be instantiated and called as such:
+# obj = MyCalendarTwo()
+# param_1 = obj.book(start,end)
