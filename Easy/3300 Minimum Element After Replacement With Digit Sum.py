3a8627ba869c0fd64f360a6e3a3c61f507cbda67
ARNAB DAS <raj713335@gmail.com>
2024-09-28 21:10:36 +0530

    Create 3300 Minimum Element After Replacement With Digit Sum.py

---
+++ Easy/3300 Minimum Element After Replacement With Digit Sum.py
@@ -0,0 +1,13 @@
+
+
+class Solution:
+    def minElement(self, nums: List[int]) -> int:
+        
+        for i in range(0, len(nums)):
+            sumx = 0
+            for each in str(nums[i]):
+                sumx += int(each)
+            nums[i] = sumx
+            
+        return min(nums)
+        
