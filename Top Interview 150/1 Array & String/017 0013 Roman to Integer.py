28849495f4aa60a2dc3a3ce7957cf0e63e62d4d2
ARNAB DAS <raj713335@gmail.com>
2024-11-21 05:59:04 +0530

    Update 017 0013 Roman to Integer.py

--- Top Interview 150/1 Array & String/017 0013 Roman to Integer.py
+++ Top Interview 150/1 Array & String/017 0013 Roman to Integer.py
@@ -2,24 +2,18 @@
 
 class Solution:
     def romanToInt(self, s: str) -> int:
+
         roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
         
         res = 0
-        flag = False
         
         for i in range(0, len(s)):
-
             if i+1 < len(s) and roman[s[i]] < roman[s[i+1]]:
-                flag = True
-                continue
-
-            if flag == True:
-                res += (roman[s[i]] - roman[s[i-1]])
-                flag = False
-                continue
-            if not flag:
+                res -= roman[s[i]]
+            else:
                 res += roman[s[i]]
             
         return res
+        
             
         
