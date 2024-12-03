64e49a4cc14f54561ec91f03ebad2eeecbfe893e
ARNAB DAS <raj713335@gmail.com>
2024-12-04 02:53:47 +0530

    Update 025 0125 Valid Palindrome.py

--- Top Interview 150/2 Two Pointers/025 0125 Valid Palindrome.py
+++ Top Interview 150/2 Two Pointers/025 0125 Valid Palindrome.py
@@ -1,12 +1,25 @@
 # https://leetcode.com/problems/valid-palindrome/description/?envType=study-plan-v2&envId=top-interview-150
 
-
-import re
-
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        
+
         s = s.lower()
-        m = re.sub(r'[^a-z0-9]*','', s)
-        
-        return m[::-1]==m
+        n = len(s)
+
+        i, j = 0, n-1
+
+        while i <= j:
+
+            while i < j and not s[i].isalnum():
+                i += 1
+
+            while i < j and not s[j].isalnum():
+                j -= 1
+
+            if s[i] != s[j]:
+                return False
+            
+            i += 1
+            j -= 1
+
+        return True
