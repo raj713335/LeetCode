828fccfa117470717803d4848f509ffc1afd8ea8
ARNAB DAS <raj713335@gmail.com>
2024-12-08 03:16:12 +0530

    Create 032 0030 Substring with Concatenation of All Words.py

---
+++ Top Interview 150/3 Sliding Window/032 0030 Substring with Concatenation of All Words.py
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/substring-with-concatenation-of-all-words/description/?envType=study-plan-v2&envId=top-interview-150
+
+class Solution:
+    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+        
+        dictx = {}
+        res = []
+        n = len(words[0])
+        total_words = n * len(words)
+
+        for each in words:
+            if each in dictx.keys():
+                dictx[each] += 1
+            else:
+                dictx[each] = 1
+
+        for i in range(0, len(s) - total_words + 1):
+            temp_dictx = dictx.copy()
+            for j in range(i, total_words + i, n):
+                temp = s[j:j+n]
+                if temp not in temp_dictx.keys():
+                    break
+                else:
+                    temp_dictx[temp] -= 1
+
+            if set(temp_dictx.values()) == {0}:
+                res.append(i)
+
+        return res
+
