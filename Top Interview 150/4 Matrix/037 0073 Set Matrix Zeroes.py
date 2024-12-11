02db793c6c01050a4841d22a38e050b3ac2329e8
ARNAB DAS <raj713335@gmail.com>
2024-12-11 20:07:05 +0530

    Create 037 0073 Set Matrix Zeroes.py

---
+++ Top Interview 150/4 Matrix/037 0073 Set Matrix Zeroes.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/set-matrix-zeroes/description/?envType=study-plan-v2&envId=top-interview-150
+
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        
+        col_dictx = {}
+        
+        row_dictx = {}
+        
+        
+        for i in range(0, len(matrix)):
+            for j in range(0, len(matrix[0])):
+                if matrix[i][j] == 0:
+                    if i not in col_dictx:
+                        col_dictx[i] = 1
+                    if j not in row_dictx:
+                        row_dictx[j] = 1
+                        
+        for i in range(0, len(matrix)):
+            for j in range(0, len(matrix[0])):
+                if i in col_dictx or j in row_dictx:
+                    matrix[i][j] = 0
+        
+        
