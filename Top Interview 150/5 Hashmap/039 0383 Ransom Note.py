14e3fbfa4d33ab8d700bde6b2ccff51d493eb120
ARNAB DAS <raj713335@gmail.com>
2024-12-12 12:23:20 +0530

    Update 039 0383 Ransom Note.py

--- Top Interview 150/5 Hashmap/039 0383 Ransom Note.py
+++ Top Interview 150/5 Hashmap/039 0383 Ransom Note.py
@@ -1,3 +1,5 @@
+# https://leetcode.com/problems/ransom-note/?envType=study-plan-v2&envId=top-interview-150
+
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         
