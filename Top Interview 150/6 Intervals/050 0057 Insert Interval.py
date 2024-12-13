07a19b8319aa932421391b89e9bb8de67bc90932
ARNAB DAS <raj713335@gmail.com>
2024-12-14 04:31:27 +0530

    Create 050 0057 Insert Interval.py

---
+++ Top Interview 150/6 Intervals/050 0057 Insert Interval.py
@@ -0,0 +1,31 @@
+# https://leetcode.com/problems/insert-interval/?envType=study-plan-v2&envId=top-interview-150
+
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        
+        intervals.append(newInterval)
+        intervals = sorted(intervals, key = lambda x: x[0])
+        
+        res = []
+        
+        i = 0
+
+        while i < len(intervals):
+            
+            start = intervals[i]
+
+            while i + 1 < len(intervals) and intervals[i+1][0] <= start[1]:
+                if start[1] < intervals[i + 1][1]:
+                    start[1] = intervals[i + 1][1]
+                i += 1
+
+            if i + 1 < len(intervals) and intervals[i+1][0] <= start[1]:
+                res.append([start[0], intervals[i+1][1]])
+                i += 1
+            else:
+                res.append([start[0], start[1]]) 
+
+            i += 1
+        
+        return res
+                    
