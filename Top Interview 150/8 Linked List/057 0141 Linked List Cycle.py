19860b458e81f34cfd8f5c54e57c1d604c9c067b
ARNAB DAS <raj713335@gmail.com>
2024-12-22 02:07:57 +0530

    Add files via upload

---
+++ Top Interview 150/8 Linked List/057 0141 Linked List Cycle.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/linked-list-cycle/description/?envType=study-plan-v2&envId=top-interview-150
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+
+        slow, fast = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if fast == slow:
+                return True
+
+        return False
+
+
+        
