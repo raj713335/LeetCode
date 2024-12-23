44444a0a1963e8fcfc402ef9c24120ece9abe3b6
ARNAB DAS <raj713335@gmail.com>
2024-12-23 23:25:58 +0530

    Create 0148 Sort List.py

---
+++ Medium/0148 Sort List.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/sort-list/description/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        dummy = ListNode()
+        curr = dummy
+        stack = []
+
+        while head:
+            val = head.val
+            stack.append(val)
+            head = head.next
+        
+        stack = sorted(stack)
+
+        for each in stack:
+            curr.next = ListNode(each)
+            curr = curr.next
+
+        return dummy.next
+        
