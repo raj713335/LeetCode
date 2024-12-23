3deb625f971307531b863dddd719748bb85ec3cc
ARNAB DAS <raj713335@gmail.com>
2024-12-24 03:01:09 +0530

    Update 0025 Reverse Nodes in k-Group.py

--- Hard/0025 Reverse Nodes in k-Group.py
+++ Hard/0025 Reverse Nodes in k-Group.py
@@ -1,25 +1,56 @@
-# https://leetcode.com/problems/reverse-nodes-in-k-group/
+# https://leetcode.com/problems/reverse-nodes-in-k-group/description
 
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+
+# class Solution:
+#     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+#         l, node = 0, head
+#         while node:
+#             l += 1
+#             node = node.next
+#         if k <= 1 or l < k:
+#             return head
+#         node, cur = None, head
+#         for _ in range(k):
+#             nxt = cur.next
+#             cur.next = node
+#             node = cur
+#             cur = nxt
+#         head.next = self.reverseKGroup(cur, k)
+#         return node
+
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        l, node = 0, head
-        while node:
-            l += 1
-            node = node.next
-        if k <= 1 or l < k:
-            return head
-        node, cur = None, head
-        for _ in range(k):
-            nxt = cur.next
-            cur.next = node
-            node = cur
-            cur = nxt
-        head.next = self.reverseKGroup(cur, k)
-        return node
+
+        dummy = ListNode()
+        curr = dummy
+        
+        while head:
+            stack = []
+            balance = k
+            length = 0
+            while head and balance > 0:
+                
+                stack.append(head.val)
+                head = head.next
+                length += 1
+                balance -= 1
+                
+            if len(stack) == k:
+                for each in stack[::-1]:
+                    curr.next = ListNode(each)
+                    curr = curr.next
+            else:
+                for each in stack:
+                    curr.next = ListNode(each)
+                    curr = curr.next
+
+        return dummy.next
+        
 
         
