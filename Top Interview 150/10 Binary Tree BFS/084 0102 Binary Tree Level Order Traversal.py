aa35657e82d963b8539301d3c5309c69c133013e
ARNAB DAS <raj713335@gmail.com>
2025-01-15 03:06:57 +0530

    Update 084 0102 Binary Tree Level Order Traversal.py

--- Top Interview 150/10 Binary Tree BFS/084 0102 Binary Tree Level Order Traversal.py
+++ Top Interview 150/10 Binary Tree BFS/084 0102 Binary Tree Level Order Traversal.py
@@ -1,37 +1,34 @@
-# https://leetcode.com/problems/binary-tree-level-order-traversal/description/
-
-
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+import collections
+
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-
+        
         if not root:
             return []
 
-        stack = collections.deque([root])
-
+        q = collections.deque([root])
         res = []
 
-        while stack:
+        while q:
             temp = []
-            for _ in range(0, len(stack)):
-                
-                curr = stack.popleft()
+            for _ in range(0, len(q)):
+
+                curr = q.popleft()
                 temp.append(curr.val)
 
-
                 if curr.left:
-                    stack.append(curr.left)
-                
+                    q.append(curr.left)
+
                 if curr.right:
-                    stack.append(curr.right)
-            
+                    q.append(curr.right)
+
             res.append(temp)
 
         return res
-        
