bcd7c4bece92637720fa237638add4eea1ec304f
ARNAB DAS <raj713335@gmail.com>
2025-01-22 03:37:54 +0530

    Create 0051 N-Queens.py

---
+++ Hard/0051 N-Queens.py
@@ -0,0 +1,37 @@
+# https://leetcode.com/problems/n-queens/description/
+
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+
+        col = set()
+        posDiag = set()
+        negDiag = set()
+
+        res = []
+        board = [["."] * n for i in range(n)]
+
+        def backtrack(r):
+            if r == n:
+                copy = ["".join(row) for row in board]
+                res.append(copy)
+                return 
+
+            for c in range(n):
+                if c in col or (r + c) in posDiag or (r - c) in negDiag:
+                    continue
+
+                col.add(c)
+                posDiag.add(r + c)
+                negDiag.add(r - c)
+                board[r][c] = "Q"
+
+                backtrack(r + 1)
+
+                col.remove(c)
+                posDiag.remove(r + c)
+                negDiag.remove(r - c)
+                board[r][c] = "."
+
+        
+        backtrack(0)
+        return res
