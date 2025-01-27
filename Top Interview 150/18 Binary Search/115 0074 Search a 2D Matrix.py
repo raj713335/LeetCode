606be83e7867f4c34e598442e34b755ed7dbc30c
ARNAB DAS <raj713335@gmail.com>
2025-01-27 23:04:05 +0530

    Update 115 0074 Search a 2D Matrix.py

--- Top Interview 150/18 Binary Search/115 0074 Search a 2D Matrix.py
+++ Top Interview 150/18 Binary Search/115 0074 Search a 2D Matrix.py
@@ -2,35 +2,32 @@
 
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        
-        row, col = len(matrix), len(matrix[0])
 
-        top, bot = 0, row - 1
+        l, r = 0, len(matrix[0]) - 1
 
-        while top <= bot:
-            row = (top + bot) // 2
-            if target > matrix[row][-1]:
+        top, bottom = 0, len(matrix) - 1
+
+        while top <= bottom:
+            row = (top + bottom) // 2
+
+            if matrix[row][0] > target:
+                bottom = row - 1
+            elif matrix[row][-1] < target:
                 top = row + 1
-            elif target < matrix[row][0]:
-                bot = row - 1
             else:
                 break
 
-        if not (top <= bot):
+        if top > bottom:
             return False
-        
-        row = (top + bot) // 2
 
-        left, right = 0, col
+        while l <= r:
+            mid = (l + r) // 2
 
-        while left <= right:
-            mid = (left + right) // 2
-
-            if matrix[row][mid] == target:
-                return True
-            elif matrix[row][mid] < target:
-                left = mid + 1
+            if matrix[row][mid] < target:
+                l = mid + 1
             elif matrix[row][mid] > target:
-                right = mid - 1
+                r = mid - 1
+            else:
+                return True
 
         return False
