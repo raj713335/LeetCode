7ca2725e65609033c7032b3338353e74fd3bcbc4
ARNAB DAS <raj713335@gmail.com>
2025-01-31 02:17:41 +0530

    Update 118 0034 Find First and Last Position of Element in Sorted Array.py

--- Top Interview 150/18 Binary Search/118 0034 Find First and Last Position of Element in Sorted Array.py
+++ Top Interview 150/18 Binary Search/118 0034 Find First and Last Position of Element in Sorted Array.py
@@ -1,54 +1,38 @@
 # https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
 
 
-import math 
-
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        left = 0
-        right = len(nums)-1
-
-        while left <= right:
-            mid = (right + left) // 2
-
-            if nums[mid] == target:
-                break
-            elif nums[mid] > target:
-                right = mid - 1
-            elif nums[mid] < target:
-                left = mid + 1
-
-        if not (left <= right):
-            return [-1, -1]
-
-        mid = (left + right) // 2
-        left, right = mid, mid
-
-        print(left, right)
-
-        while left > 0:
-            try:
-                left -= 1
-                if nums[left] == target:
-                    continue
-                else:
-                    left += 1
-                    break
-            except:
-                left += 1
-                break
         
-        while right < len(nums):
-            try:
-                right += 1
-                if nums[right] == target:
-                    continue
-                else:
-                    right -= 1
-                    break
-            except:
-                right -= 1
-                break
 
-        return [left, right]
-            
+        l, r = 0, len(nums) - 1
+
+        def search_index(mid):
+            left, right = mid, mid
+
+            while left-1 >= 0:
+                if nums[left-1] == target:
+                    left -= 1
+                else:
+                    break
+
+            while right + 1 < len(nums):
+                if nums[right+1] == target:
+                    right += 1
+                else:
+                    break
+
+            return [left, right]
+
+        while l <= r:
+            mid = (l+r)//2
+
+            if nums[mid] < target:
+                l = mid + 1
+            elif nums[mid] > target:
+                r = mid - 1
+            else:
+                return search_index(mid)
+
+        return [-1, -1]
+        
