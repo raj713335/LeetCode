4c4dcb941330b5c96e06673dc516cc30088d8a1e
ARNAB DAS <raj713335@gmail.com>
2025-02-02 04:11:46 +0530

    Update 120 0004 Median of Two Sorted Arrays.py

--- Top Interview 150/18 Binary Search/120 0004 Median of Two Sorted Arrays.py
+++ Top Interview 150/18 Binary Search/120 0004 Median of Two Sorted Arrays.py
@@ -1,60 +1,37 @@
 # https://leetcode.com/problems/median-of-two-sorted-arrays/description/
 
-import math
-
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         
-        total_count = len(nums1)+len(nums2)
-        flag=False
+        A = nums1
+        B = nums2
 
-        
-        listx = []
-        
-        if total_count%2 ==0:
-            val = total_count // 2
-            flag = True
-            count = -1
-        else:
-            val = math.ceil(total_count/2)
-            count = 0
-            
-        i, j = 0, 0
-        
-        while (i < len(nums1) and j < len(nums2)):
-            if nums1[i] < nums2[j]:
-                listx.append(nums1[i])
-                count += 1
-                i+= 1
+        total = len(A) + len(B)
+        half = total // 2 
+
+        if len(B) < len(A):
+            A, B = B, A
+
+        l, r = 0, len(A) - 1
+
+        while True:
+            i = (l + r) // 2
+            j = half - i - 2
+
+            Aleft = A[i] if i >= 0 else float('-infinity')
+            Aright = A[i+1] if (i + 1) < len(A) else float('infinity')
+            Bleft = B[j] if j >= 0 else float('-infinity')
+            Bright = B[j+1] if (j+1) < len(B) else float('infinity')
+
+            if Aleft <= Bright and Bleft <= Aright:
+                if total % 2 != 0:
+                    return min(Aright, Bright)
+                else:
+                    return (max(Aleft, Bleft) + min(Aright, Bright))/2
+            elif Aleft > Bright:
+                r = i - 1
             else:
-                listx.append(nums2[j])
-                count += 1
-                j+=1
-            if count == val:
-                if flag == False:
-                    return listx[-1]
-                else:
-                    return ((listx[-1]+listx[-2])/2)
-                                             
-        while(i< len(nums1) and j == len(nums2)):
-            listx.append(nums1[i])
-            count += 1
-            i += 1
-            if count == val:
-                if flag == False:
-                    return listx[-1]
-                else:
-                    return ((listx[-1]+listx[-2])/2)
-                                             
-        while(i== len(nums1) and j < len(nums2)):
-            listx.append(nums2[j])
-            count += 1
-            j += 1
-            if count == val:
-                if flag == False:
-                    return listx[-1]
-                else:
-                    return ((listx[-1]+listx[-2])/2)
+                l = i + 1
                                              
         
                 
