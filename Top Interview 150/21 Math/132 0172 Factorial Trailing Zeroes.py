2a752e095805458099df6b4af3d2a0f327ae8704
ARNAB DAS <raj713335@gmail.com>
2025-02-04 01:38:58 +0530

    Update 132 0172 Factorial Trailing Zeroes.py

--- Top Interview 150/21 Math/132 0172 Factorial Trailing Zeroes.py
+++ Top Interview 150/21 Math/132 0172 Factorial Trailing Zeroes.py
@@ -1,21 +1,32 @@
 # https://leetcode.com/problems/factorial-trailing-zeroes/description/
 
-import sys
-sys.set_int_max_str_digits(0)
+# import sys
+# sys.set_int_max_str_digits(0)
 
 class Solution:
     def trailingZeroes(self, n: int) -> int:
         
-        val = 1
+        # val = 1
         
-        for i in range(1, n+1):
-            val *= i
+        # for i in range(1, n+1):
+        #     val *= i
         
-        count = 0
+        # count = 0
         
-        for each in str(val)[::-1]:
-            if each == "0":
-                count += 1
-            else:
-                return int(count)
+        # for each in str(val)[::-1]:
+        #     if each == "0":
+        #         count += 1
+        #     else:
+        #         return int(count)
+
+
+        res = 0
+
+        i = 5
+
+        while i <= n:
+            res = res + (n // i)
+            i = i * 5
         
+        return res
+
