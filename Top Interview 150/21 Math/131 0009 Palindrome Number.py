67a1c69eaae65e416e570158b74b9c9fb10d421c
ARNAB DAS <raj713335@gmail.com>
2025-02-03 16:07:14 +0530

    Add files via upload

---
+++ Top Interview 150/21 Math/131 0009 Palindrome Number.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/palindrome-number/description/
+
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if str(x)[::-1] == str(x):
+            return True
+        else:
+            return False
+        
