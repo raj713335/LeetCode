0951b9fb923a641ade3d13c9aa534fc83f9dbf6a
ARNAB DAS <raj713335@gmail.com>
2025-02-04 01:02:45 +0530

    Update 132 0066 Plus One.py

--- Top Interview 150/21 Math/132 0066 Plus One.py
+++ Top Interview 150/21 Math/132 0066 Plus One.py
@@ -3,13 +3,27 @@
 
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
+
+        res = 0
+
+        for num in digits:
+            res = res * 10 + num
+
+        res = res + 1
+
+        res_arr = []
+
+        for num in str(res):
+            res_arr.append(int(num))
+
+        return res_arr
         
         
-        output = ""
-        for each in digits:
-            output += str(each)
+        # output = ""
+        # for each in digits:
+        #     output += str(each)
             
-        output = int(output)+1
+        # output = int(output)+1
         
-        return list(map(int, str(output)))
+        # return list(map(int, str(output)))
         
