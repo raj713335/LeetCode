c42355424c218f65d8d0c86f7b829335ecf9eec3
ARNAB DAS <raj713335@gmail.com>
2025-02-05 04:17:33 +0530

    Update 129 0137 Single Number II.py

--- Top Interview 150/20 Bit Manipulation/129 0137 Single Number II.py
+++ Top Interview 150/20 Bit Manipulation/129 0137 Single Number II.py
@@ -2,23 +2,41 @@
 
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        
-        result = 0
+
+        res = 0
 
         for k in range(0, 32):
             temp = (1 << k)
-
             countOnes = 0
 
             for num in nums:
-                if (num & temp) != 0:
+                if (temp & num) != 0:
                     countOnes += 1
-            
+
             if countOnes % 3 == 1:
-                result = result | temp
+                res = (res | temp)
 
-        # Handle negative numbers
-        if result >= (1 << 31):  # If sign bit is set
-            result -= (1 << 32)
+        if res >= (1 << 31):
+            res = res - (1 << 32)
 
-        return result 
+        return res
+        
+        # result = 0
+
+        # for k in range(0, 32):
+        #     temp = (1 << k)
+
+        #     countOnes = 0
+
+        #     for num in nums:
+        #         if (num & temp) != 0:
+        #             countOnes += 1
+            
+        #     if countOnes % 3 == 1:
+        #         result = result | temp
+
+        # # Handle negative numbers
+        # if result >= (1 << 31):  # If sign bit is set
+        #     result -= (1 << 32)
+
+        # return result 
