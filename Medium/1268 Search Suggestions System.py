d2bf4f8b1e7c258e725a824940abce97e2dc174b
ARNAB DAS <raj713335@gmail.com>
2025-02-22 21:05:00 +0530

    Update 1268 Search Suggestions System.py

--- Medium/1268 Search Suggestions System.py
+++ Medium/1268 Search Suggestions System.py
@@ -23,3 +23,40 @@ class Solution:
                 res[-1].append(products[l+j])
         
         return res
+
+
+# with Prefix Tree  
+
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.suggestions = []
+
+class Solution:
+    def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
+        
+        root = TrieNode()
+
+        products = sorted(products)
+
+        for word in products:
+            curr = root
+            for w in word:
+                if w not in curr.children:
+                    curr.children[w] = TrieNode()
+                curr = curr.children[w]
+                if len(curr.suggestions) < 3:
+                    curr.suggestions.append(word)
+
+
+        res = [[] for char in searchWord]
+        
+        curr = root
+        for i, w in enumerate(searchWord):
+            if w not in curr.children:
+                break
+            curr = curr.children[w]
+            res[i] = curr.suggestions
+
+        return res
