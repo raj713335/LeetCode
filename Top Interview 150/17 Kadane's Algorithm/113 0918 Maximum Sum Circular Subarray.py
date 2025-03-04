79f42911373b063c3ad309c0121e3918ce4b059a
ARNAB DAS <raj713335@gmail.com>
2025-03-05 02:56:44 +0530

    Update 113 0918 Maximum Sum Circular Subarray.py

--- Top Interview 150/17 Kadane's Algorithm/113 0918 Maximum Sum Circular Subarray.py
+++ Top Interview 150/17 Kadane's Algorithm/113 0918 Maximum Sum Circular Subarray.py
@@ -2,17 +2,25 @@
 
 class Solution:
     def maxSubarraySumCircular(self, nums: List[int]) -> int:
+        
+        def kadane(nums):
 
-        globalMax, globalMin = nums[0], nums[0]
-        currMax, currMin = 0, 0
-        total = 0
+            max_sum, curr_sum = nums[0], nums[0]
 
-        for n in nums:
-            currMax = max(currMax + n, n)
-            currMin = min(currMin + n, n)
-            total += n
-            globalMax = max(globalMax, currMax)
-            globalMin = min(globalMin, currMin)
+            for num in nums[1:]:
+                curr_sum = max(num, curr_sum + num)
+                max_sum = max(max_sum, curr_sum)
 
-        return max(globalMax, total - globalMin) if globalMax > 0 else globalMax
+            return max_sum
 
+        
+        max_kadane = kadane(nums)
+        
+        total_sum = sum(nums)
+
+        min_kadane = kadane([-num for num in nums])
+
+        if total_sum == -min_kadane:
+            return max_kadane
+
+        return max(max_kadane, total_sum+min_kadane)
