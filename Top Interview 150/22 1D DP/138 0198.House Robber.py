0a6ceb7e9b5b1969352be201c9e29dff7a77d5f6
ARNAB DAS <raj713335@gmail.com>
2025-03-12 01:33:49 +0530

    Update 138 0198.House Robber.py

--- Top Interview 150/22 1D DP/138 0198.House Robber.py
+++ Top Interview 150/22 1D DP/138 0198.House Robber.py
@@ -1,5 +1,24 @@
 # https://leetcode.com/problems/house-robber/description/
 
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+
+
+        def minCost(i, memo={}):
+            if i in memo:
+                return memo[i]
+            if i < 2:
+                return cost[i]
+            memo[i] = cost[i] + min(minCost(i-1, memo), minCost(i-2, memo))
+            return memo[i]
+        
+        n = len(cost)
+        return min(minCost(n-1), minCost(n-2))
+
+
+# Alternative Solution 
+
 class Solution:
     def rob(self, nums: List[int]) -> int:
 
