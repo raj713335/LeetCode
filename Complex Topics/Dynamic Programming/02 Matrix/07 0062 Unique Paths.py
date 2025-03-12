73d37ef487079740111092f5409b5046c7ad12fa
ARNAB DAS <raj713335@gmail.com>
2025-03-13 00:37:00 +0530

    Create 07 0062 Unique Paths.py

---
+++ Complex Topics/Dynamic Programming/02 Matrix/07 0062 Unique Paths.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/unique-paths/description/
+
+class Solution:
+    def uniquePaths(self, m: int, n: int, memo={}) -> int:
+
+        if (m, n) in memo:
+            return memo[m, n]
+        elif m == 1 and n == 1:
+            return 1 
+        elif m == 0 or n == 0:
+            return 0
+        else:
+            memo[m, n] = self.uniquePaths(m-1, n, memo) + self.uniquePaths(m, n-1, memo)
+
+        return memo[m, n]
