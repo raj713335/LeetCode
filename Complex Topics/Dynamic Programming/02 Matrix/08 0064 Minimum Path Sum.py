0cdf481991d2337b1e964b5c05b1262bdf684949
ARNAB DAS <raj713335@gmail.com>
2025-03-13 01:10:43 +0530

    Update 08 0064 Minimum Path Sum.py

--- Complex Topics/Dynamic Programming/02 Matrix/08 0064 Minimum Path Sum.py
+++ Complex Topics/Dynamic Programming/02 Matrix/08 0064 Minimum Path Sum.py
@@ -1,5 +1,27 @@
 # https://leetcode.com/problems/minimum-path-sum/description/
 
+
+# Time Complexity: O(m × n) (where m is ROW and n is COL)
+# Space Complexity: O(m × n) (can be optimized to O(n))
+
+
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+
+        ROW, COL = len(grid), len(grid[0])
+
+        res = [[float("inf")] * (COL+1) for c in range(0, ROW+1)]
+
+        res[ROW-1][COL] = 0
+
+        for i in range(ROW-1, -1, -1):
+            for j in range(COL-1, -1, -1):
+                res[i][j] = grid[i][j] + min(res[i+1][j], res[i][j+1])
+
+        return res[0][0]
+
+
+
 # Time Complexity: O(m × n)
 # Space Complexity: O(m × n) (due to memo storage and recursion stack)
 
