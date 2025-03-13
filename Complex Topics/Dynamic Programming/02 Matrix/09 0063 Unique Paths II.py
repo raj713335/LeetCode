c59f7905178c4763d882a8b5cd966400e85d4770
ARNAB DAS <raj713335@gmail.com>
2025-03-13 15:35:05 +0530

    Update 09 0063 Unique Paths II.py

--- Complex Topics/Dynamic Programming/02 Matrix/09 0063 Unique Paths II.py
+++ Complex Topics/Dynamic Programming/02 Matrix/09 0063 Unique Paths II.py
@@ -10,15 +10,18 @@ class Solution:
         m, n = len(obstacleGrid), len(obstacleGrid[0])
 
         def dfs(i, j, memo = {}):
+
             if (i, j) in memo:
                 return memo[i, j]
-            if i == m-1 and j == n-1:
+
+            elif i == 0 and j == 0:
                 return 1 if  obstacleGrid[i][j] == 0 else 0
             
-            if i < 0 or i >= m or j < 0 or j >= n or obstacleGrid[i][j] == 1:
+            elif i < 0 or j < 0 or obstacleGrid[i][j] == 1:
                 return 0
             else:
-                memo[i, j] = dfs(i+1, j, memo) + dfs(i, j+1, memo)
+                memo[i, j] = dfs(i-1, j, memo) + dfs(i, j-1, memo)
             return memo[i, j]
 
-        return dfs(0, 0)
+        return dfs(m-1, n-1)
+
