d7524c3366d3601444ae6f310146bd8e23c351a2
ARNAB DAS <raj713335@gmail.com>
2025-04-07 01:59:05 +0530

    Create 0494 Target Sum.py

---
+++ Medium/0494 Target Sum.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/target-sum/description/
+
+
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+
+        n = len(nums)
+
+        memo = {}
+
+        def backtrack(i, cur_sum):
+
+            if i == n:
+                if cur_sum == target:
+                    return 1
+                else:
+                    return 0
+
+            if (i, cur_sum) in memo:
+                return memo[(i, cur_sum)]
+
+            memo[(i, cur_sum)] = backtrack(i+1, cur_sum + nums[i]) + backtrack(i+1, cur_sum - nums[i])
+            return memo[(i, cur_sum)]
+
+        return backtrack(0, 0)
