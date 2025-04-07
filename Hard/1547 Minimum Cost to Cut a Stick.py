5549c9e0c38d87d74f97686c6729a5cad69f2367
ARNAB DAS <raj713335@gmail.com>
2025-04-07 18:51:08 +0530

    Create 1547 Minimum Cost to Cut a Stick.py

---
+++ Hard/1547 Minimum Cost to Cut a Stick.py
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/minimum-cost-to-cut-a-stick/description/
+
+
+class Solution:
+    def minCost(self, n: int, cuts: List[int]) -> int:
+
+        memo = {}
+
+        def dfs(l, r):
+
+            if (l, r) in memo:
+                return memo[(l, r)]
+
+            if r - l == 1:
+                return 0
+
+            res = float("inf")
+
+            for c in cuts:
+                if l < c < r:
+                    res = min(res, (r - l) + dfs(l, c) + dfs(c, r))
+
+            memo[(l, r)] = res = 0 if res == float("inf") else res
+
+            return res
+
+        return dfs(0, n)
+
+        
