43fbc57ad215593617627596d485adf4d2c7306a
ARNAB DAS <raj713335@gmail.com>
2025-04-08 09:30:10 +0530

    Update 42 0322 Coin Change.py

--- Complex Topics/Dynamic Programming/09 General 1D/42 0322 Coin Change.py
+++ Complex Topics/Dynamic Programming/09 General 1D/42 0322 Coin Change.py
@@ -1,5 +1,6 @@
 # https://leetcode.com/problems/coin-change/description/
 
+# Top Down Approach 
 
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
@@ -27,3 +28,27 @@ class Solution:
 
         res  =  dfs(amount)
         return res if res != float('inf') else -1
+
+
+# Bottom Up Approach 
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+
+        n = len(coins)
+
+        dp = [float("inf")] * (amount + 1)
+
+        dp[0] = 0
+
+        for i in range(1, amount+1):
+            for coin in coins:
+                if i - coin  >= 0:
+                    dp[i] = min(dp[i], dp[i-coin] + 1)
+
+        if dp[amount] != float("inf"):
+            return dp[amount]
+        else:
+            return -1
+
+        
