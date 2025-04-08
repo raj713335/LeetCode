754ee0c0bd8c0a9978d936bfd465360ad05cd9c3
ARNAB DAS <raj713335@gmail.com>
2025-04-08 16:59:11 +0530

    Create 0518 Coin Change II.py

---
+++ Medium/0518 Coin Change II.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/coin-change-ii/description/
+
+class Solution:
+    def change(self, amount: int, coins: List[int], memo = {}) -> int:
+        
+        n = len(coins)
+        dp = {}
+
+        def dfs(rem, i):
+            if rem == 0:
+                return 1
+            elif rem < 0 or i == n:
+                return 0
+            elif (rem, i) in dp:
+                return dp[(rem, i)]
+
+
+            dp[(rem, i)] = dfs(rem - coins[i], i) + dfs(rem, i + 1)
+            return dp[(rem, i)]
+
+        return dfs(amount, 0)    
