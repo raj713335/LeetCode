62a1690c6defd22f1e32679b988a4cc1bced9564
ARNAB DAS <raj713335@gmail.com>
2025-04-09 04:08:30 +0530

    Update 0322 Coin Change.py

--- Medium/0322 Coin Change.py
+++ Medium/0322 Coin Change.py
@@ -1,5 +1,8 @@
 # https://leetcode.com/problems/coin-change/description/
 
+
+# Bottom Up Approach
+
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
 
@@ -15,5 +18,40 @@ class Solution:
             return dp[amount]
         else:
             return -1 
+
+# Top Down Approach 
+
+# Time Complexity: O(amount * n) (where amount is the target value and n is the number of coin denominations)
+# Space Complexity: O(amount) (due to the memoization dictionary `dp` and the recursion stack)
+
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+
+        n = len(coins)
+
+        dp = {}
+
+        def dfs(value):
+
+            if value > amount:
+                return float('inf')
+            elif value == amount:
+                return 0
+            elif value in dp:
+                return dp[value]
+
+            min_ways = float('inf')
+
+            for coin in coins:
+                min_ways = min(min_ways, 1 + dfs(value + coin))
+
+            dp[value] = min_ways
+
+            return dp[value]
+
+        res = dfs(0)
+
+        return res if res != float('inf') else -1 
 
         
