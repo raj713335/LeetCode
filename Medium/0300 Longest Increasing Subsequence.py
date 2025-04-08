91f0e9f1d1be1a3a2b53df20bb628f8b09279a13
ARNAB DAS <raj713335@gmail.com>
2025-04-09 04:07:06 +0530

    Update 0300 Longest Increasing Subsequence.py

--- Medium/0300 Longest Increasing Subsequence.py
+++ Medium/0300 Longest Increasing Subsequence.py
@@ -1,5 +1,8 @@
 # https://leetcode.com/problems/longest-increasing-subsequence/
 
+
+# Bottom Up Approach
+
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
 
@@ -12,6 +15,35 @@ class Solution:
                     LIS[i] = max(LIS[i], 1 + LIS[j])
 
         return max(LIS)
+
+
+# Top Down Approach 
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+
+        n = len(nums)
+
+        dp = {}
+
+        def dfs(index, prev):
+
+            if index == n:
+                return 0
+
+            elif index in dp:
+                return dp[index]
+
+            max_length = 0 
+
+            for i in range(index, n):
+                if nums[i] > prev:
+                    max_length = max(max_length, 1 + dfs(i, nums[i]))
+
+            dp[index] = max_length
+            return dp[index]
+
+        return dfs(0, float('-inf'))
             
         
         
