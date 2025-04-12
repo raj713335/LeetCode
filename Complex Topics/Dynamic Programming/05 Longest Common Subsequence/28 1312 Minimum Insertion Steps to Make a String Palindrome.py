04f647a5ad8329cf4bfa593ae5ebb5aee8da5627
ARNAB DAS <raj713335@gmail.com>
2025-04-12 18:16:38 +0530

    Create 28 1312 Minimum Insertion Steps to Make a String Palindrome.py

---
+++ Complex Topics/Dynamic Programming/05 Longest Common Subsequence/28 1312 Minimum Insertion Steps to Make a String Palindrome.py
@@ -0,0 +1,31 @@
+# https://leetcode.com/problems/minimum-insertion-steps-to-make-a-string-palindrome/description/
+
+class Solution:
+    def minInsertions(self, s: str) -> int:
+
+        n = len(s) - 1
+
+        dp = {}
+
+        def dfs(s1, s2, n1, n2):
+
+            if n1 == -1 or n2 == -1:
+                return 0
+            if (n1, n2) in dp:
+                return dp[(n1, n2)]
+
+            max_length = 0
+
+            if s1[n1] == s2[n2]:
+                max_length = max(max_length, 1 + dfs(s1, s2, n1-1, n2-1))
+            else:
+                left = dfs(s1, s2, n1-1, n2)
+                right = dfs(s1, s2, n1, n2-1)
+
+                max_length = max(max_length, left, right)
+
+            dp[(n1, n2)] = max_length
+            return dp[(n1, n2)]
+
+        return (n + 1) - dfs(s, s[::-1],n ,n)
+        
