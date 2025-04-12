1940180c2afa2b6b3958eec0c7230fbfb58688fb
ARNAB DAS <raj713335@gmail.com>
2025-04-12 16:03:19 +0530

    Update 1092 Shortest Common Supersequence .py

--- Hard/1092 Shortest Common Supersequence .py
+++ Hard/1092 Shortest Common Supersequence .py
@@ -1,5 +1,53 @@
 # https://leetcode.com/problems/shortest-common-supersequence/description/
 
+
+## Bottom Up Approach 
+
+class Solution:
+    def shortestCommonSupersequence(self, str1: str, str2: str) -> str:
+
+        n = len(str1)
+        m = len(str2)
+
+        res = []
+
+        dp = [[0] * (m + 1) for _ in range(n+1)]
+
+        for i in range(1, n+1):
+            for j in range(1, m+1):
+                if str1[i - 1] == str2[j - 1]:
+                    dp[i][j] = 1 + dp[i-1][j-1]
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+        i, j = n, m
+
+        while i > 0 and j > 0:
+            if str1[i-1] == str2[j-1]:
+                res.append(str1[i-1])
+                i -= 1
+                j -= 1
+        
+            elif dp[i - 1][j] > dp[i][j - 1]:
+                res.append(str1[i - 1])
+                i -= 1
+            else:
+                res.append(str2[j - 1])
+                j -= 1
+
+
+        while i > 0:
+            res.append(str1[i - 1])
+            i -= 1
+        while j > 0:
+            res.append(str2[j - 1])
+            j -= 1
+
+        return "".join(res[::-1])
+
+        
+
+
 ## Recursive + Memoization Approach
 class Solution:
     def shortestCommonSupersequence(self, str1: str, str2: str) -> str:
@@ -68,49 +116,4 @@ class Solution:
             return dp[(index_1, index_2)]
 
         return dfs(str1, str2, n1, n2)
-        
-## Bottom Up Approach 
-
-class Solution:
-    def shortestCommonSupersequence(self, str1: str, str2: str) -> str:
-
-        n = len(str1)
-        m = len(str2)
-
-        res = []
-
-        dp = [[0] * (m + 1) for _ in range(n+1)]
-
-        for i in range(1, n+1):
-            for j in range(1, m+1):
-                if str1[i - 1] == str2[j - 1]:
-                    dp[i][j] = 1 + dp[i-1][j-1]
-                else:
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-
-        i, j = n, m
-
-        while i > 0 and j > 0:
-            if str1[i-1] == str2[j-1]:
-                res.append(str1[i-1])
-                i -= 1
-                j -= 1
-        
-            elif dp[i - 1][j] > dp[i][j - 1]:
-                res.append(str1[i - 1])
-                i -= 1
-            else:
-                res.append(str2[j - 1])
-                j -= 1
-
-
-        while i > 0:
-            res.append(str1[i - 1])
-            i -= 1
-        while j > 0:
-            res.append(str2[j - 1])
-            j -= 1
-
-        return "".join(res[::-1])
-
         
