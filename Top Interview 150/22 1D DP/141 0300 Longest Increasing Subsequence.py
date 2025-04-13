0d40f659caa856f8bad454009fe0e464380d09bd
ARNAB DAS <raj713335@gmail.com>
2025-04-13 13:22:01 +0530

    Update 141 0300 Longest Increasing Subsequence.py

--- Top Interview 150/22 1D DP/141 0300 Longest Increasing Subsequence.py
+++ Top Interview 150/22 1D DP/141 0300 Longest Increasing Subsequence.py
@@ -3,6 +3,7 @@
 # Time Complexity: O(n^2) (where n is the length of the input list `nums`, due to memoization over (index, prev) pairs and looping from index to n)
 # Space Complexity: O(n^2) (due to the memoization dictionary and the recursion stack)
 
+# Recursive + Memoization Approach 
 
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
@@ -29,5 +30,24 @@ class Solution:
             return dp[index]
 
         return dfs(0, float('-inf'))
+
+
+# Bottom-Up Approach
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+
+        n = len(nums)
+
+        LIS = [1] * (n)
+
+        for i in range(n):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    LIS[i] = max(LIS[i], 1 + LIS[j])
+
+        return max(LIS)
+            
+
 
       
