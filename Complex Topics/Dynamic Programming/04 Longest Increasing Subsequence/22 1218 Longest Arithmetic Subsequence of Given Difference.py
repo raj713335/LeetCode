24966927496685e18ba814f53354ec4ee7d6550a
ARNAB DAS <raj713335@gmail.com>
2025-04-13 23:01:27 +0530

    Create 22 1218 Longest Arithmetic Subsequence of Given Difference.py

---
+++ Complex Topics/Dynamic Programming/04 Longest Increasing Subsequence/22 1218 Longest Arithmetic Subsequence of Given Difference.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/longest-arithmetic-subsequence-of-given-difference/description/
+
+
+class Solution:
+    def longestSubsequence(self, arr: List[int], difference: int) -> int:
+
+        """
+        dp is a hashtable, dp[x] is the longest subsequence ending with number x
+        """
+
+        dp = {}
+
+        for x in arr:
+            if x - difference in dp:
+                dp[x] = dp[x-difference] + 1
+            else:
+                dp[x] = 1
+
+        return max(dp.values())
+        
+
+        
