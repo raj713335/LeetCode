ca193d569af31d2b23fd89c5760460ec0347d244
ARNAB DAS <raj713335@gmail.com>
2025-04-13 22:54:20 +0530

    Update 0673 Number of Longest Increasing Subsequence.py

--- Medium/0673 Number of Longest Increasing Subsequence.py
+++ Medium/0673 Number of Longest Increasing Subsequence.py
@@ -1,5 +1,37 @@
 # https://leetcode.com/problems/number-of-longest-increasing-subsequence/description/
 
+## Tabulation Method 
+
+class Solution:
+    def findNumberOfLIS(self, nums: List[int]) -> int:
+
+        n = len(nums)
+
+        LIS = [[1, 1] for _ in range(n)]
+
+        for i in range(n):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    if LIS[j][0] + 1 > LIS[i][0]:
+                        LIS[i][0] = LIS[j][0] + 1
+                        LIS[i][1] = LIS[j][1]
+                    elif LIS[j][0] + 1 == LIS[i][0]:
+                        LIS[i][1] += LIS[j][1]
+
+        max_LIS = 0
+        count = 0
+
+        for each in LIS:
+            if each[0] > max_LIS:
+                max_LIS = each[0]
+                count = each[1]
+            elif each[0] == max_LIS:
+                count += each[1]
+
+        return count
+        
+# Recursive + Memoization Method
+
 class Solution:
     def findNumberOfLIS(self, nums: List[int]) -> int:
 
