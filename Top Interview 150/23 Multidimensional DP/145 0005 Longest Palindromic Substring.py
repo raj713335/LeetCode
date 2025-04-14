4c1d03d76d35b8c8dc3835b99b1c182b4d0623b9
ARNAB DAS <raj713335@gmail.com>
2025-04-14 13:52:17 +0530

    Create 145 0005 Longest Palindromic Substring.py

---
+++ Top Interview 150/23 Multidimensional DP/145 0005 Longest Palindromic Substring.py
@@ -0,0 +1,42 @@
+# https://leetcode.com/problems/longest-palindromic-substring/description/
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+
+        n = len(s)
+
+        # Edge case: empty or single character string
+        if n < 2:
+            return s
+
+        dp = [[False] * (n) for s in range(n)]
+
+        start = 0           # Start index of the longest palindrome
+        max_length = 1      # Length of the longest palindrome
+
+        # All substrings of length 1 are palindromes
+        for i in range(n):
+            dp[i][i] = 1
+
+        # Check for substrings of length 2
+        for i in range(0, n-1):
+            if s[i] == s[i+1]:
+                dp[i][i+1] = 1 
+                start = i
+                max_length = 2
+
+
+        # Check for substrings of length 3 and more
+        for length in range(3, n + 1): # length of substring
+            for i in range(n - length + 1):
+                j = i + length -1 # ending index of substring
+
+                # Check if the current substring is a palindrome
+                if s[i] == s[j] and dp[i+1][j-1] == True:
+                    dp[i][j] = True
+                    start = i
+                    max_length = length
+
+        # Return the longest palindromic substring
+        return s[start:start+max_length]
+        
