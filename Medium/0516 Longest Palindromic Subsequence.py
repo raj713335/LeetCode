9b38a0b3753b561f7226b13371d4a8c74172a7c7
ARNAB DAS <raj713335@gmail.com>
2025-04-14 12:53:10 +0530

    Update 0516 Longest Palindromic Subsequence.py

--- Medium/0516 Longest Palindromic Subsequence.py
+++ Medium/0516 Longest Palindromic Subsequence.py
@@ -1,5 +1,7 @@
 # https://leetcode.com/problems/longest-palindromic-subsequence/description/
 
+## Recursive + Memoization
+
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
 
@@ -28,5 +30,46 @@ class Solution:
             return dp[(n1, n2)]
 
         return dfs(s, s[::-1],n ,n)
+
+        
+## Tabulation Method / Bottom Up Approach 
+
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
+
+        str1 = s
+        str2 = s[::-1]
+
+        n = len(str1)
+        m = len(str2)
+
+        res = []
+
+        dp = [[0] * (m + 1) for _ in range(n+1)]
+
+        for i in range(1, n+1):
+            for j in range(1, m+1):
+                if str1[i - 1] == str2[j - 1]:
+                    dp[i][j] = 1 + dp[i-1][j-1]
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+        i, j = n, m
+
+        while i > 0 and j > 0:
+            if str1[i-1] == str2[j-1]:
+                res.append(str1[i-1])
+                i -= 1
+                j -= 1
+        
+            elif dp[i - 1][j] > dp[i][j - 1]:
+
+                i -= 1
+            else:
+
+                j -= 1
+
+
+        return len(res)
 
         
