7c55ba2afb3c5a624beccabe4d6539b28577ec6b
ARNAB DAS <raj713335@gmail.com>
2025-04-14 12:13:56 +0530

    Create 1964 Find the Longest Valid Obstacle Course at Each Position.py

---
+++ Hard/1964 Find the Longest Valid Obstacle Course at Each Position.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/find-the-longest-valid-obstacle-course-at-each-position/description/
+
+class Solution:
+    def longestObstacleCourseAtEachPosition(self, obstacles: List[int]) -> List[int]:
+
+        res = [] # res[i] = height of LIS ending at i & including nums[i]
+        dp = [float("inf")] * (len(obstacles)+1) #dp[i], where i = length of LIS, is the smallest ending value
+        for obs in obstacles:
+            index = bisect.bisect(dp, obs)
+            res.append(index+1)
+            dp[index] = obs
+
+        return res
+        
