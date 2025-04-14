f874fd79c546bd24daa40ea225719e03740ddf61
ARNAB DAS <raj713335@gmail.com>
2025-04-15 03:30:38 +0530

    Update 145 0005 Longest Palindromic Substring.py

--- Top Interview 150/23 Multidimensional DP/145 0005 Longest Palindromic Substring.py
+++ Top Interview 150/23 Multidimensional DP/145 0005 Longest Palindromic Substring.py
@@ -42,4 +42,23 @@ class Solution:
 
         # Return the longest palindromic substring
         return s[start:start+max_length]
+
+
+# Recursive Approach 
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+
+        def expand(s,i,j):
+            while i>=0 and j<len(s) and s[i]==s[j]:
+                i-=1
+                j+=1
+            return s[i+1:j]
+
+        ans=''
+        for i in range(len(s)):
+            ans=max(ans,expand(s,i,i), expand(s,i,i+1), key=len)
+        return ans
+            
+    
         
