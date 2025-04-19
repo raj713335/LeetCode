a12fb7f1a82176d926e445049ab2ee534c31ca09
ARNAB DAS <raj713335@gmail.com>
2025-04-19 23:41:22 +0530

    Create 11 0392 Is Subsequence.py

---
+++ LeetCode 75/02 Two Pointers/11 0392 Is Subsequence.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/is-subsequence/description/
+
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+
+        n1 = len(s)
+        n2 = len(t)
+
+        if n1 == 0:
+            return True
+
+        i = 0
+        j = 0 
+
+        while j < n2 and i < n1:
+            if s[i] == t[j]:
+                i += 1
+                j += 1
+            else:
+                j += 1
+
+        if i == n1:
+            return True
+        else:
+            return False
+        
