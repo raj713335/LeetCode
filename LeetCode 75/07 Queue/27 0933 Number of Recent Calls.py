020e7428f4382b1b71e96ce208e08b85fd2c7042
ARNAB DAS <raj713335@gmail.com>
2025-04-20 00:41:14 +0530

    Add files via upload

---
+++ LeetCode 75/07 Queue/27 0933 Number of Recent Calls.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/number-of-recent-calls/description/
+
+
+class RecentCounter:
+
+    def __init__(self):
+        self.s = []
+        
+
+    def ping(self, t: int) -> int:
+        
+        while self.s and t - self.s[0] > 3000:
+            self.s.pop(0)  # remove 1st el if it's 3000+ away from t
+        self.s.append(t)
+        return len(self.s) 
+        
+
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
+            
+        
