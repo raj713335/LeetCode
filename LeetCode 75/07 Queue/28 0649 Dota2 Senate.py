f3ee6b4223276dc76a841491dfc05d46bc6acbcd
ARNAB DAS <raj713335@gmail.com>
2025-04-20 00:42:16 +0530

    Create 28 0649 Dota2 Senate.py

---
+++ LeetCode 75/07 Queue/28 0649 Dota2 Senate.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/dota2-senate/description/
+
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+
+        n = len(senate)
+        D = collections.deque()
+        R = collections.deque()
+        for i, c in enumerate(senate):
+            if c == 'R':
+                R.append(i)
+            else:
+                D.append(i)
+        
+
+        while R and D:
+            r,d = R.popleft(), D.popleft()
+            if r < d:
+                R.append(r + n)
+            else:
+                D.append(d + n)
+
+        return "Radiant" if R else "Dire"
