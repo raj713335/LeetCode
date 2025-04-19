671fa582b311a599b787ee7befb9e17174c2f08e
ARNAB DAS <raj713335@gmail.com>
2025-04-19 23:42:51 +0530

    Create 12 0011 Container With Most Water.py

---
+++ LeetCode 75/02 Two Pointers/12 0011 Container With Most Water.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/container-with-most-water/description/
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+
+        max_vol = 0
+
+        i, j = 0, len(height) - 1
+
+        while i < j:
+
+            temp_vol = min(height[i], height[j]) * (j-i)
+
+            if temp_vol > max_vol:
+                max_vol = temp_vol
+
+            if height[j] > height[i]:
+                i += 1
+            else:
+                j -= 1
+        
+        return max_vol
+        
