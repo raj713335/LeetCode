6c040493b2c34f9da6e58929e8fc0876c826cc72
ARNAB DAS <raj713335@gmail.com>
2025-04-19 23:58:00 +0530

    Add files via upload

---
+++ LeetCode 75/04 Prefix Sum/18 1732 Find the Highest Altitude.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/find-the-highest-altitude/description/
+
+
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        val = 0
+        max_alt = 0
+        
+        for i in range(0, len(gain)):
+            val += gain[i]
+            if max_alt < val:
+                max_alt = val
+                
+        return max_alt
+        
