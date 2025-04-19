7d439d8b21512e89056e1623daa3255154c61c6c
ARNAB DAS <raj713335@gmail.com>
2025-04-19 17:33:26 +0530

    Create 09 0443 String Compression.py

---
+++ LeetCode 75/01 Array  String/09 0443 String Compression.py
@@ -0,0 +1,35 @@
+# https://leetcode.com/problems/string-compression/description/
+
+import copy
+
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+
+        prev = ""
+        count = 0
+
+        res = ""
+        
+        for i in range(0, len(chars)):
+            if chars[i] != prev:
+                if prev != "":
+                    if count == 1:
+                        res += prev
+                    else:
+                        res += prev + str(count)
+                    
+                prev = chars[i]
+                count = 1
+            else:
+                count += 1
+
+
+        if count == 1:
+            res += prev
+        else:
+            res += prev + str(count)
+
+        chars[:] = res
+
+        return len(chars)
+        
