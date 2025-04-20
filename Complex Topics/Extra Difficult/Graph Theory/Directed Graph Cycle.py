20a75259e9f5a51ae8fef1d6bc5683f648281f03
ARNAB DAS <raj713335@gmail.com>
2025-04-20 11:39:22 +0530

    Add files via upload

---
+++ Complex Topics/Extra Difficult/Graph Theory/Directed Graph Cycle.py
@@ -0,0 +1,62 @@
+# https://www.geeksforgeeks.org/problems/detect-cycle-in-a-directed-graph/1
+
+
+from collections import defaultdict
+
+class Solution:
+    def isCycle(self, V, edges):
+        
+        adjency_list = defaultdict(list)
+		
+        for u, v in edges:
+            adjency_list[u].append(v)
+            
+        visited = set()
+        current_path = set()
+
+        def dfs(node, adjency_list, visited, current_path):
+            visited.add(node)
+            current_path.add(node)
+            
+            for next in adjency_list[node]:
+                if next not in visited:
+                    ans = dfs(next, adjency_list, visited, current_path)
+                    if ans:
+                        return True
+                elif next in visited and next in current_path:
+                    return True
+                    
+            current_path.remove(node)
+            return False
+
+        for node in range(V):
+            if node not in visited:
+                ans = dfs(node, adjency_list, visited, current_path)
+                if ans:
+                    return True
+        return False
+
+#{ 
+ # Driver Code Starts
+from collections import deque
+
+
+def main():
+    tc = int(input())
+    for _ in range(tc):
+        V = int(input())
+        E = int(input())
+        edges = []
+        for _ in range(E):
+            u, v = map(int, input().split())
+            edges.append((u, v))
+
+        obj = Solution()
+        ans = obj.isCycle(V, edges)
+        print("true" if ans else "false")
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
