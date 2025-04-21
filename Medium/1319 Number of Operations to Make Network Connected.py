717093c97f84fd931936f34ae7b461a98a6ceade
ARNAB DAS <raj713335@gmail.com>
2025-04-21 12:16:33 +0530

    Create 1319 Number of Operations to Make Network Connected.py

---
+++ Medium/1319 Number of Operations to Make Network Connected.py
@@ -0,0 +1,39 @@
+# https://leetcode.com/problems/number-of-operations-to-make-network-connected/description/
+
+
+from collections import defaultdict
+
+class Solution:
+    def makeConnected(self, n: int, connections: List[List[int]]) -> int:
+
+        if len(connections) < n - 1:
+            return -1
+
+        adjency_list = defaultdict(list)
+
+        for u, v in connections:
+            adjency_list[u].append(v)
+            adjency_list[v].append(u)
+
+
+        def dfs(i):
+
+            visited.add(i)
+
+            for nei in adjency_list[i]:
+                if nei not in visited:
+                    dfs(nei)
+
+
+
+        components = 0
+        visited = set()
+
+        for i in range(n):
+            if i not in visited:
+                dfs(i)
+                components += 1
+
+
+        return components - 1
+        
