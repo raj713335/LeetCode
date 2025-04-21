bb19c585d8426018b2600ba0932abdda77a3f004
ARNAB DAS <raj713335@gmail.com>
2025-04-21 17:55:28 +0530

    Create 1928 Minimum Cost to Reach Destination in Time.py

---
+++ Hard/1928 Minimum Cost to Reach Destination in Time.py
@@ -0,0 +1,40 @@
+# https://leetcode.com/problems/minimum-cost-to-reach-destination-in-time/description/
+
+
+import heapq
+from collections import defaultdict
+
+class Solution:
+    def minCost(self, maxTime: int, edges: List[List[int]], passingFees: List[int]) -> int:
+
+        n = len(passingFees)
+
+        # Build graph: city -> [(neighbor, time)]
+        graph = defaultdict(list)
+        for u, v, time in edges:
+            graph[u].append((v, time))
+            graph[v].append((u, time))
+
+        # (cost, city, time)
+        pq = [(passingFees[0], 0, 0)]
+        
+        # minTime[i] = minimum time we reached city i
+        minTime = [float('inf')] * n
+        minTime[0] = 0
+
+        while pq:
+            cost, city, time = heapq.heappop(pq)
+
+            if city == n - 1:
+                return cost
+            
+            for nei, t in graph[city]:
+                newTime = time + t
+                newCost = cost + passingFees[nei]
+
+                if newTime <= maxTime and newTime < minTime[nei]:
+                    minTime[nei] = newTime
+                    heapq.heappush(pq, (newCost, nei, newTime))
+
+        return -1
+        
