9f76bb4d860a533d9c1c39651687c9b8cd923ace
ARNAB DAS <raj713335@gmail.com>
2025-04-22 23:39:20 +0530

    Create 59 1137 N-th Tribonacci Number.py

---
+++ LeetCode 75/17 DP - 1D/59 1137 N-th Tribonacci Number.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/n-th-tribonacci-number/description/
+
+class Solution:
+    def tribonacci(self, n: int, memo = {}) -> int:
+
+        if n in memo:
+            return memo[n]
+
+        elif n == 0:
+            return 0
+        elif n == 1:
+            return 1
+        elif n == 2:
+            return 1
+        else:
+            memo[n] = self.tribonacci(n-1, memo) + self.tribonacci(n-2, memo) + self.tribonacci(n-3, memo)
+        
+        return memo[n]
+        
