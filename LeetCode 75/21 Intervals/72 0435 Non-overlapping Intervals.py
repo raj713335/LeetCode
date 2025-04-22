8d037fc129c7f58fa086a996979e2226a106ba16
ARNAB DAS <raj713335@gmail.com>
2025-04-22 23:23:32 +0530

    Add files via upload

---
+++ LeetCode 75/21 Intervals/72 0435 Non-overlapping Intervals.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/non-overlapping-intervals/description/
+
+
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+
+        intervals.sort()
+        res = 0
+
+        prevEnd = intervals[0][1]
+        for start, end in intervals[1:]:
+            if start >= prevEnd:
+                prevEnd = end
+            else:
+                res += 1
+                prevEnd = min(prevEnd, end)
+        
+        return res
