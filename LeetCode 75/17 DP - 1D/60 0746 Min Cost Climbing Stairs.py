c233aa10de2e897c5235beba1ddaf48713bb5d6a
ARNAB DAS <raj713335@gmail.com>
2025-04-22 23:38:21 +0530

    Create 60 0746 Min Cost Climbing Stairs.py

---
+++ LeetCode 75/17 DP - 1D/60 0746 Min Cost Climbing Stairs.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/min-cost-climbing-stairs/description/
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+
+
+        def minCost(i, memo={}):
+            if i in memo:
+                return memo[i]
+            if i < 2:
+                return cost[i]
+            memo[i] = cost[i] + min(minCost(i-1, memo), minCost(i-2, memo))
+            return memo[i]
+        
+        n = len(cost)
+        return min(minCost(n-1), minCost(n-2))
+        
