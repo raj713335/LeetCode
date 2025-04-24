9e29bcfb0051033bab5cc1a1e53dcd29bba45c50
ARNAB DAS <raj713335@gmail.com>
2025-04-24 23:57:51 +0530

    Create 54 2300 Successful Pairs of Spells and Potions.py

---
+++ LeetCode 75/15 Binary Search/54 2300 Successful Pairs of Spells and Potions.py
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/successful-pairs-of-spells-and-potions/description/
+
+
+class Solution:
+    def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
+
+        potions = sorted(potions)
+        res = []
+
+        for spell in spells:
+
+            l, r = 0, len(potions)-1
+            index = len(potions)
+
+            while (l <= r):
+                mid = (l+r)//2
+
+                if potions[mid] * spell >= success:
+                    r = mid-1
+                    index = mid
+                else:
+                    l = mid+1
+
+            res.append(len(potions)-index)
+
+        return res
+                    
+
+            
+
