8afe47ba1b7cb09dcfcd7bb4e7b1137b4b6b6a77
ARNAB DAS <raj713335@gmail.com>
2025-04-26 03:05:31 +0530

    Create 44 0547 Number of Provinces.py

---
+++ LeetCode 75/12 Graphs - DFS/44 0547 Number of Provinces.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/number-of-provinces/description/
+
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+
+        cities_len = len(isConnected)
+        provience = 0
+        visited = set()
+
+        def dfs(start):
+
+            visited.add(start)
+
+            for i in range(0, cities_len):
+                if isConnected[start][i] == 1 and i not in visited:
+                    dfs(i)
+
+        for i in range(0, cities_len):
+                if i not in visited:
+                    dfs(i)
+                    provience += 1
+
+        return provience
+        
