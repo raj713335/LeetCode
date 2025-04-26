a8d2760939bfca550c249eea17edcacc47bf9f07
ARNAB DAS <raj713335@gmail.com>
2025-04-27 02:19:03 +0530

    Add files via upload

---
+++ Complex Topics/Binary Search/5 Tricky Invariant/20 1539 Kth Missing Positive Number.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/kth-missing-positive-number/description/
+
+
+class Solution:
+    def findKthPositive(self, arr: List[int], k: int) -> int:
+        
+        beg, end = 0, len(arr)
+        while beg < end:
+            mid = (beg + end) // 2
+            if arr[mid] - mid - 1 < k:
+                beg = mid + 1
+            else:
+                end = mid
+        return end + k
+        
