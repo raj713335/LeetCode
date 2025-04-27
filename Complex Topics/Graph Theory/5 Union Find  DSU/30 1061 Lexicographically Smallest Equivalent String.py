23d283d2123e9f4f8f50edac8505cf96cbc23019
ARNAB DAS <raj713335@gmail.com>
2025-04-27 15:25:46 +0530

    Update 30 1061 Lexicographically Smallest Equivalent String.py

--- Complex Topics/Graph Theory/5 Union Find  DSU/30 1061 Lexicographically Smallest Equivalent String.py
+++ Complex Topics/Graph Theory/5 Union Find  DSU/30 1061 Lexicographically Smallest Equivalent String.py
@@ -5,6 +5,44 @@
 
 class UnionFind:
     def __init__(self):
+        self.parent = {} # For 'a' to 'z'
+
+    def find(self, x):
+        if x != self.parent.setdefault(x, x):
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX == rootY:
+            return
+        if rootX < rootY:
+            self.parent[rootY] = rootX
+        else:
+            self.parent[rootX] = rootY
+
+
+class Solution:
+    def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
+
+        uf = UnionFind()
+    
+        for a, b in zip(s1, s2):
+            uf.union(a, b)
+        
+        result = []
+        for ch in baseStr:
+            smallest = uf.find(ch)
+            result.append(smallest)
+        
+        return ''.join(result)
+
+
+
+
+class UnionFind:
+    def __init__(self):
         self.parent = list(range(26))  # 'a' to 'z'
 
     def find(self, x):
