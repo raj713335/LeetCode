caa1d7c4d404af06af06075bac1022b5df55ad38
ARNAB DAS <raj713335@gmail.com>
2025-05-12 12:36:00 +0530

    Create 07 1068 Product Sales Analysis I.sql

---
+++ SQL 50/2 Basic Joins/07 1068 Product Sales Analysis I.sql
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/product-sales-analysis-i/description/
+
+# Write your MySQL query statement below
+
+SELECT P.product_name, S.year, S.price
+FROM Sales S
+INNER JOIN
+Product P 
+ON S.product_id = P.product_id 
