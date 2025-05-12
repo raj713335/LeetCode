f5f1934a9a2b1efdacbe38701a3f114e772ca4ff
ARNAB DAS <raj713335@gmail.com>
2025-05-12 12:38:08 +0530

    Create 09 0197 Rising Temperature.sql

---
+++ SQL 50/2 Basic Joins/09 0197 Rising Temperature.sql
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/rising-temperature/description/
+
+/* Write your PL/SQL query statement below */
+
+
+SELECT wt1.id 
+FROM Weather wt1, Weather wt2
+WHERE wt1.temperature > wt2.temperature AND 
+TO_DAYS(wt1.recordDate)-TO_DAYS(wt2.recordDate)=1;
