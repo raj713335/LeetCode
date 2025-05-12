b374f5735484225ca44bc45eb7008c7168c6db26
ARNAB DAS <raj713335@gmail.com>
2025-05-12 20:09:56 +0530

    Create 0176 Second Highest Salary.sql

---
+++ Medium/0176 Second Highest Salary.sql
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/second-highest-salary/description/
+
+# Write your MySQL query statement below
+
+SELECT (
+  SELECT DISTINCT salary
+  FROM Employee
+  ORDER BY salary DESC
+  LIMIT 1 OFFSET 1
+) AS SecondHighestSalary;
