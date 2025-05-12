dbb456d137f385f849ac1ff7053714c474a3209b
ARNAB DAS <raj713335@gmail.com>
2025-05-12 12:41:51 +0530

    Create 12 1280 Students and Examinations.sql

---
+++ SQL 50/2 Basic Joins/12 1280 Students and Examinations.sql
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/students-and-examinations/description/
+
+# Write your MySQL query statement below
+
+SELECT student.student_id,student.student_name,subject.subject_name,COUNT(exam.subject_name) as attended_exams
+FROM Students as student
+JOIN Subjects as subject
+LEFT JOIN Examinations as exam
+ON student.student_id=exam.student_id AND subject.subject_name=exam.subject_name
+GROUP BY student.student_id,subject.subject_name
+ORDER BY student_id,subject_name;
