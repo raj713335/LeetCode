ee62609373ee07ea20591561924fc56191833b35
ARNAB DAS <raj713335@gmail.com>
2025-05-12 12:31:19 +0530

    Create 03 0595 Big Countries.py

---
+++ SQL 50/1 Select/03 0595 Big Countries.py
@@ -0,0 +1,6 @@
+# https://leetcode.com/problems/big-countries/description/
+
+# Write your MySQL query statement below
+
+SELECT name, population, area FROM World
+WHERE population >= 25000000 OR area >= 3000000 
