c90220f76621656c017397de28b6be8d821ec9ca
ARNAB DAS <raj713335@gmail.com>
2025-05-14 21:11:32 +0530

    Update concurrent_function.py

--- Python Concepts/concurrent_function.py
+++ Python Concepts/concurrent_function.py
@@ -1,3 +1,7 @@
+# Time Complexity: O(n) in terms of setup, but O(t) in practice due to concurrency
+# Overall, effectively O(t) latency-wise with n threads.
+
+
 import requests
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
@@ -49,3 +53,9 @@ if __name__ == "__main__":
     result = health_check(endpoints)
     for url, info in result.items():
         print(f"{url}: {info}")
+
+
+# https://does-not-exist.typicode.com: {'status_code': None, 'ok': False, 'error': 'HTTPSConnectionPool(host=\'does-not-exist.typicode.com\', port=443): Max retries exceeded with url: / (Caused by NameResolutionError("<urllib3.connection.HTTPSConnection object at 0x0000021C820C9690>: Failed to resolve \'does-not-exist.typicode.com\' ([Errno 11001] getaddrinfo failed)"))'}
+# https://api.github.com: {'status_code': 200, 'ok': True, 'response_time': 0.269436}
+# https://jsonplaceholder.typicode.com/posts: {'status_code': 200, 'ok': True, 'response_time': 0.283378}
+# https://httpbin.org/get: {'status_code': 200, 'ok': True, 'response_time': 5.190144}
