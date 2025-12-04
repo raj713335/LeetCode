9511fa96b3caca769493ee5b2d45ac088ea95936
ARNAB DAS <raj713335@gmail.com>
2025-12-04 17:16:19 +0530

    Create SQL query for cities in each state
    Add SQL query to find cities grouped by state.

---
+++ Easy/3198 Find Cities in Each State.sql
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/find-cities-in-each-state/
+
+# Write your MySQL query statement below
+
+
+select 
+    state, 
+    group_concat(city order by city separator ', ') as cities 
+from cities 
+group by state 
+order by state
