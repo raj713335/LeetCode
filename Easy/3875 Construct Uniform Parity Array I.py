2cb9070de7f6288cf853204a49af1d62c9e0a0af
ARNAB DAS <raj713335@gmail.com>
2026-03-22 14:28:02 +0530

    Add initial implementation of uniformArray method

---
+++ Easy/3875 Construct Uniform Parity Array I.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/construct-uniform-parity-array-i/description/
+
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+
+        return True
+        
