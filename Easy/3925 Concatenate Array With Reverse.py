5453e5ab2313beefe3c532e5d4169382b685fd91
ARNAB DAS <raj713335@gmail.com>
2026-05-10 13:15:28 +0530

    Create 3925 Concatenate Array With Reverse.py

---
+++ Easy/3925 Concatenate Array With Reverse.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/concatenate-array-with-reverse/description/
+
+class Solution:
+    def concatWithReverse(self, nums: list[int]) -> list[int]:
+
+        return nums + nums[::-1]
+        
